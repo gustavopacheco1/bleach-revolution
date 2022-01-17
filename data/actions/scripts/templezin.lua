15c6f1ab9bc50403ddcb34ad24c96a31ff70b10d
Gustavo <gustavopacheco61@yahoo.com>
2022-01-17 00:35:45 -0300

    organizando e atualizando actions para OTX2

--- data/actions/scripts/templezin.lua
+++
@@ -1,5 +0,0 @@
-function onUse(cid)
-   if getPlayerLevel(cid) >= 1 then
-	doCreatureSay(cid, "Aaaahh, estou sem chakra!", TALKTYPE_MONSTER)
- end
-end
--- data/actions/scripts/testarhidan.lua
+++
@@ -1,8 +0,0 @@
-function onUse(cid, item, frompos, item2, topos)
-		if not isPlayer(cid) then
-		return false
-		end
-		if getPlayerLevel(cid) > 1 then
-		doCreatureSay(cid, "-l NTOUnLiMiTeD l-", TALKTYPE_MONSTER)
-	end
-end
