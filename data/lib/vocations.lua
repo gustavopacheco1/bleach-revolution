17539e910beeda784f5bc5129ca6388e0d5bdf18
magojackson <enriquejackson66@gmaiil.com>
2022-01-18 13:35:02 -0300

    de 6 transform pra 5

--- data/lib/vocations.lua
+++ data/lib/vocations.lua
@@ -2,130 +2,118 @@ VOCATION_TRANSFORM = {
     -- Ichigo Kurosaki
     ["Ichigo Kurosaki"] = {
         [1] = {vocation = 1, looktype = 2, effect = {number = 10}},
-        [100] = {vocation = 2, looktype = 3, effect = {number = 10}},
-        [150] = {vocation = 3, looktype = 4, effect = {number = 10}},
-        [200] = {vocation = 4, looktype = 5, effect = {number = 10}},
-        [300] = {vocation = 5, looktype = 6, effect = {number = 10}},
-        [400] = {vocation = 6, looktype = 6, effect = {number = 10}},
+        [150] = {vocation = 2, looktype = 3, effect = {number = 10}},
+        [250] = {vocation = 3, looktype = 4, effect = {number = 10}},
+        [350] = {vocation = 4, looktype = 5, effect = {number = 10}},
+        [500] = {vocation = 5, looktype = 6, effect = {number = 10}},
+        
     },
 
     -- Chad
     ["Chad"] = {
         [1] = {vocation = 7, looktype = 157, effect = {number = 10}},
-        [100] = {vocation = 8, looktype = 157, effect = {number = 10}},
-        [150] = {vocation = 9, looktype = 158, effect = {number = 10}},
-        [200] = {vocation = 10, looktype = 159, effect = {number = 10}},
-        [300] = {vocation = 11, looktype = 160, effect = {number = 10}},
-        [400] = {vocation = 12, looktype = 161, effect = {number = 10}},
-    },
+        [150] = {vocation = 8, looktype = 158, effect = {number = 10}},
+        [250] = {vocation = 9, looktype = 159, effect = {number = 10}},
+        [350] = {vocation = 10, looktype = 160, effect = {number = 10}},
+        [500] = {vocation = 11, looktype = 161, effect = {number = 10}},
+	},
 
     -- Bazz-B
     ["Bazz-B"] = {
         [1] = {vocation = 13, looktype = 148, effect = {number = 10}},
-        [100] = {vocation = 14, looktype = 148, effect = {number = 10}},
-        [150] = {vocation = 15, looktype = 149, effect = {number = 10}},
-        [200] = {vocation = 16, looktype = 150, effect = {number = 10}},
-        [300] = {vocation = 17, looktype = 151, effect = {number = 10}},
-        [400] = {vocation = 18, looktype = 152, effect = {number = 10}},
+        [150] = {vocation = 14, looktype = 149, effect = {number = 10}},
+        [250] = {vocation = 15, looktype = 150, effect = {number = 10}},
+        [350] = {vocation = 16, looktype = 151, effect = {number = 10}},
+        [500] = {vocation = 17, looktype = 152, effect = {number = 10}},  
     },
 
     -- Orihime Inoue
     ["Orihime Inoue"] = {
         [1] = {vocation = 19, looktype = 59, effect = {number = 10}},
-        [100] = {vocation = 20, looktype = 59, effect = {number = 10}},
-        [150] = {vocation = 21, looktype = 60, effect = {number = 10}},
-        [200] = {vocation = 22, looktype = 61, effect = {number = 10}},
-        [300] = {vocation = 23, looktype = 62, effect = {number = 10}},
-        [400] = {vocation = 24, looktype = 63, effect = {number = 10}},
+        [150] = {vocation = 20, looktype = 60, effect = {number = 10}},
+        [250] = {vocation = 21, looktype = 61, effect = {number = 10}},
+        [350] = {vocation = 22, looktype = 62, effect = {number = 10}},
+        [500] = {vocation = 23, looktype = 63, effect = {number = 10}},
     },
 
     -- Uryu Ishida
     ["Uryu Ishida"] = {
         [1] = {vocation = 25, looktype = 34, effect = {number = 10}},
-        [100] = {vocation = 26, looktype = 34, effect = {number = 10}},
-        [150] = {vocation = 27, looktype = 35, effect = {number = 10}},
-        [200] = {vocation = 28, looktype = 36, effect = {number = 10}},
-        [300] = {vocation = 29, looktype = 37, effect = {number = 10}},
-        [400] = {vocation = 30, looktype = 38, effect = {number = 10}},
+        [150] = {vocation = 26, looktype = 35, effect = {number = 10}},
+        [250] = {vocation = 27, looktype = 36, effect = {number = 10}},
+        [350] = {vocation = 28, looktype = 37, effect = {number = 10}},
+        [500] = {vocation = 29, looktype = 38, effect = {number = 10}},
     },
 
     -- Ulquiorra Ciffer
     ["Ulquiorra Ciffer"] = {
         [1] = {vocation = 31, looktype = 113, effect = {number = 10}},
-        [100] = {vocation = 32, looktype = 113, effect = {number = 10}},
-        [150] = {vocation = 33, looktype = 114, effect = {number = 10}},
-        [200] = {vocation = 34, looktype = 115, effect = {number = 10}},
-        [300] = {vocation = 35, looktype = 116, effect = {number = 10}},
-        [400] = {vocation = 36, looktype = 117, effect = {number = 10}},
+        [150] = {vocation = 32, looktype = 114, effect = {number = 10}},
+        [250] = {vocation = 33, looktype = 115, effect = {number = 10}},
+        [350] = {vocation = 34, looktype = 116, effect = {number = 10}},
+        [500] = {vocation = 35, looktype = 117, effect = {number = 10}},
     },
 
     -- Zaraki Kenpachi
     ["Zaraki Kenpachi"] = {
         [1] = {vocation = 37, looktype = 123, effect = {number = 10}},
-        [100] = {vocation = 38, looktype = 123, effect = {number = 10}},
-        [150] = {vocation = 39, looktype = 124, effect = {number = 10}},
-        [200] = {vocation = 40, looktype = 125, effect = {number = 10}},
-        [300] = {vocation = 41, looktype = 126, effect = {number = 10}},
-        [400] = {vocation = 42, looktype = 127, effect = {number = 10}},
+        [150] = {vocation = 38, looktype = 124, effect = {number = 10}},
+        [250] = {vocation = 39, looktype = 125, effect = {number = 10}},
+        [350] = {vocation = 40, looktype = 126, effect = {number = 10}},
+        [500] = {vocation = 41, looktype = 127, effect = {number = 10}},
     },
 
     -- Byakuya Kuchiki
     ["Byakuya Kuchiki"] = {
         [1] = {vocation = 43, looktype = 85, effect = {number = 10}},
-        [100] = {vocation = 44, looktype = 85, effect = {number = 10}},
-        [150] = {vocation = 45, looktype = 86, effect = {number = 10}},
-        [200] = {vocation = 46, looktype = 87, effect = {number = 10}},
-        [300] = {vocation = 47, looktype = 88, effect = {number = 10}},
-        [400] = {vocation = 48, looktype = 89, effect = {number = 10}},
+        [150] = {vocation = 44, looktype = 86, effect = {number = 10}},
+        [250] = {vocation = 45, looktype = 87, effect = {number = 10}},
+        [350] = {vocation = 46, looktype = 88, effect = {number = 10}},
+        [500] = {vocation = 47, looktype = 89, effect = {number = 10}},
     },
 
     -- Toshiro Histugaya
     ["Toshiro Histugaya"] = {
         [1] = {vocation = 49, looktype = 73, effect = {number = 10}},
-        [100] = {vocation = 50, looktype = 73, effect = {number = 10}},
-        [150] = {vocation = 51, looktype = 74, effect = {number = 10}},
-        [200] = {vocation = 52, looktype = 75, effect = {number = 10}},
-        [300] = {vocation = 53, looktype = 76, effect = {number = 10}},
-        [400] = {vocation = 54, looktype = 77, effect = {number = 10}},
+        [150] = {vocation = 50, looktype = 74, effect = {number = 10}},
+        [250] = {vocation = 51, looktype = 75, effect = {number = 10}},
+        [350] = {vocation = 52, looktype = 76, effect = {number = 10}},
+        [500] = {vocation = 53, looktype = 77, effect = {number = 10}},
     },
 
     -- Rukia Kuchiki
     ["Rukia Kuchiki"] = {
         [1] = {vocation = 55, looktype = 20, effect = {number = 10}},
-        [100] = {vocation = 56, looktype = 20, effect = {number = 10}},
-        [150] = {vocation = 57, looktype = 21, effect = {number = 10}},
-        [200] = {vocation = 58, looktype = 22, effect = {number = 10}},
-        [300] = {vocation = 59, looktype = 23, effect = {number = 10}},
-        [400] = {vocation = 60, looktype = 24, effect = {number = 10}},
+        [150] = {vocation = 56, looktype = 21, effect = {number = 10}},
+        [250] = {vocation = 57, looktype = 22, effect = {number = 10}},
+        [350] = {vocation = 58, looktype = 23, effect = {number = 10}},
+        [500] = {vocation = 59, looktype = 24, effect = {number = 10}},
     },
 
     -- Nelliel Odelschwanck
     ["Nelliel Odelschwanck"] = {
         [1] = {vocation = 61, looktype = 97, effect = {number = 10}},
-        [100] = {vocation = 62, looktype = 97, effect = {number = 10}},
-        [150] = {vocation = 63, looktype = 98, effect = {number = 10}},
-        [200] = {vocation = 64, looktype = 99, effect = {number = 10}},
-        [300] = {vocation = 65, looktype = 100, effect = {number = 10}},
-        [400] = {vocation = 66, looktype = 101, effect = {number = 10}},
+        [150] = {vocation = 62, looktype = 98, effect = {number = 10}},
+        [250] = {vocation = 63, looktype = 99, effect = {number = 10}},
+        [350] = {vocation = 64, looktype = 100, effect = {number = 10}},
+        [500] = {vocation = 65, looktype = 101, effect = {number = 10}},
     },
 
     -- Retsu Unohana
     ["Retsu Unohana"] = {
         [1] = {vocation = 68, looktype = 139, effect = {number = 10}},
-        [100] = {vocation = 68, looktype = 139, effect = {number = 10}},
-        [150] = {vocation = 69, looktype = 140, effect = {number = 10}},
-        [200] = {vocation = 70, looktype = 141, effect = {number = 10}},
-        [300] = {vocation = 71, looktype = 142, effect = {number = 10}},
-        [400] = {vocation = 72, looktype = 143, effect = {number = 10}},
+        [150] = {vocation = 68, looktype = 140, effect = {number = 10}},
+        [250] = {vocation = 69, looktype = 141, effect = {number = 10}},
+        [350] = {vocation = 70, looktype = 142, effect = {number = 10}},
+        [500] = {vocation = 71, looktype = 143, effect = {number = 10}},
     },
 
     -- Renjii Abarai
     ["Renjii Abarai"] = {
         [1] = {vocation = 73, looktype = 59, effect = {number = 10}},
-        [100] = {vocation = 74, looktype = 59, effect = {number = 10}},
-        [150] = {vocation = 75, looktype = 60, effect = {number = 10}},
-        [200] = {vocation = 76, looktype = 61, effect = {number = 10}},
-        [300] = {vocation = 77, looktype = 62, effect = {number = 10}},
-        [400] = {vocation = 78, looktype = 63, effect = {number = 10}},
+        [150] = {vocation = 74, looktype = 60, effect = {number = 10}},
+        [250] = {vocation = 75, looktype = 61, effect = {number = 10}},
+        [350] = {vocation = 76, looktype = 62, effect = {number = 10}},
+        [500] = {vocation = 77, looktype = 63, effect = {number = 10}},
     },
 }
