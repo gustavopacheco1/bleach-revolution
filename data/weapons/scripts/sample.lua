0f74f68e02b5f85ddbbe1c9a82d4a471f48086d8
Gustavo <gustavopacheco61@yahoo.com>
2022-01-19 17:44:11 -0300

    organizando weapons

---
+++ data/weapons/scripts/sample.lua
@@ -0,0 +1,8 @@
+local combat = createCombatObject()
+setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
+setCombatFormula(combat, COMBAT_FORMULA_SKILL, 1, 0, 1, 0)
+
+function onUseWeapon(cid, var)
+    doSendMagicEffect(getCreaturePosition(getCreatureTarget(cid)), 30)
+	return doCombat(cid, combat, var)
+end
