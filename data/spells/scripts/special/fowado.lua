32d926ac8d8cd3a81c5ba825fac121c60bcc3d7a
Gustavo <gustavopacheco61@yahoo.com>
2022-02-05 11:55:55 -0300

    fix: fowado area

--- data/spells/scripts/special/fowado.lua
+++ data/spells/scripts/special/fowado.lua
@@ -1,7 +1,11 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_EFFECT, 20)
 
-local area = createCombatArea(AREA_SQUARE1X1)
+local area = createCombatArea({
+    {1, 1, 1},
+    {1, 3, 1},
+    {1, 1, 1},
+})
 setCombatArea(combat, area)
 
 function onTarget(cid, target)
