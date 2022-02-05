6f80fef7de1d2d0dcf088985b462269836663850
Gustavo <gustavopacheco61@yahoo.com>
2022-02-05 02:46:36 -0300

    add: add: fowado spell

---
+++ data/spells/scripts/special/fowado.lua
@@ -0,0 +1,15 @@
+local combat = createCombatObject()
+setCombatParam(combat, COMBAT_PARAM_EFFECT, 20)
+
+local area = createCombatArea(AREA_SQUARE1X1)
+setCombatArea(combat, area)
+
+function onTarget(cid, target)
+    return doChallengeCreature(cid, target)
+end
+
+setCombatCallback(combat, CALLBACK_PARAM_TARGETCREATURE, "onTarget")
+
+function onCastSpell(cid, var)
+    return doCombat(cid, combat, var)
+end
