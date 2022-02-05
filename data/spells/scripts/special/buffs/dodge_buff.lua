34f805651809924537a20f6b704470f64c6f439c
Gustavo <gustavopacheco61@yahoo.com>
2022-02-05 02:45:49 -0300

    add: spell dodge

---
+++ data/spells/scripts/special/buffs/dodge_buff.lua
@@ -0,0 +1,36 @@
+local spell = {
+    cooldown = 80,
+    duration = 15,
+    dodge_percentage = 50,
+    effect = 100
+}
+
+local combat = createCombatObject()
+local condition = createConditionObject(CONDITION_ATTRIBUTES)
+setConditionParam(condition, CONDITION_PARAM_TICKS, spell.duration * 1000)
+setConditionParam(condition, CONDITION_PARAM_BUFF, true)
+setCombatCondition(combat, condition)
+
+function onCastSpell(cid, var)
+    if exhaustion.check(cid, "special") then
+        doPlayerSendCancel(cid, "Cooldown " .. exhaustion.get(cid, "special") .. "s")
+        return false
+    end
+
+    local player_position = getCreaturePosition(cid)
+    doSendMagicEffect({
+        x = player_position.x,
+        y = player_position.y,
+        z = player_position.z
+    }, spell.effect)
+
+    setPlayerStorageValue(cid, "dodge_special", spell.dodge_percentage)
+    addEvent(function()
+        if isCreature(cid) then
+            setPlayerStorageValue(cid, "dodge_special", 0)
+        end
+    end, spell.duration * 1000)
+    exhaustion.set(cid, "special", spell.cooldown)
+    doCombat(cid, combat, var)
+    return true
+end
