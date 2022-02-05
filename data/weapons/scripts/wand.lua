8ed7468c89f2af6e43f6ad068e4ee114f8bdefb8
magojackson <97809271+magojackson@users.noreply.github.com>
2022-02-05 16:05:32 -0300

    armas found sendo feitas

--- data/weapons/scripts/wand.lua
+++ data/weapons/scripts/wand.lua
@@ -1,8 +1,8 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_HITCOLOR, COLOR_LIGHTGREEN)
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
-setCombatParam(combat, COMBAT_PARAM_EFFECT, 34)
-setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, 28)
+setCombatParam(combat, COMBAT_PARAM_EFFECT, 660)
+setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, 64)
 setCombatFormula(combat, COMBAT_FORMULA_SKILL, 1, 0, 1, 0)
 
 function onUseWeapon(cid, var)
