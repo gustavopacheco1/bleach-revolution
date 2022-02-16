5790871ae33281410755d385d90f785a569ab5cf
magojackson <97809271+magojackson@users.noreply.github.com>
2022-02-16 13:16:15 -0300

    effects novo e missile tbem, feito o combo urahara

---
+++ data/spells/scripts/attack/Urahara/Kamisori.lua
@@ -0,0 +1,20 @@
+local combat = createCombatObject()
+setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
+setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -60.0, 0, -60.0, 0)
+
+function onCastSpell(cid, var)
+    if exhaustion.check(cid, 115818) then
+        doPlayerSendCancel(cid, "You are exhausted.")
+        return false
+    end
+
+    local target_position = getCreaturePosition(getCreatureTarget(cid))
+    doSendMagicEffect({
+        x = target_position.x,
+        y = target_position.y,
+        z = target_position.z
+    }, 701)
+    
+    exhaustion.set(cid, 115818, 1)
+    return doCombat(cid, combat, var)
+end
