5ab8fbbe6590578337c5eaa36af4aec5a7c6471c
magojackson <97809271+magojackson@users.noreply.github.com>
2022-02-17 16:43:12 -0300

    mudado sprite da founder dist e dado uma arrumadinha na casa do craft items

--- data/weapons/scripts/Founder Distance.lua
+++ data/weapons/scripts/Founder Distance.lua
@@ -1,7 +1,7 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, 660)
-setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, 64)
+setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, 78)
 setCombatFormula(combat, COMBAT_FORMULA_SKILL, 1, 0, 1, 0)
 
 function onUseWeapon(cid, var)
