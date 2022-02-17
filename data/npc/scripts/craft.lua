f233c9551096e67a375fa18c9b59087c0788ea97
Gustavo <gustavopacheco61@yahoo.com>
2022-02-17 14:25:26 -0300

    fix: npc craft functions + add: multi language quando fala hi

--- data/npc/scripts/craft.lua
+++ data/npc/scripts/craft.lua
@@ -1,37 +1,58 @@
 local keywordHandler = KeywordHandler:new()
 local npcHandler = NpcHandler:new(keywordHandler)
 NpcSystem.parseParameters(npcHandler)
- 
-function onCreatureAppear(cid)         npcHandler:onCreatureAppear(cid)         end
-function onCreatureDisappear(cid)      npcHandler:onCreatureDisappear(cid)         end
-function onCreatureSay(cid, type, msg)   npcHandler:onCreatureSay(cid, type, msg:lower())   end
-function onThink()                  npcHandler:onThink()                  end
+
+function onCreatureAppear(cid)npcHandler:onCreatureAppear(cid) end
+function onCreatureDisappear(cid)npcHandler:onCreatureDisappear(cid) end
+function onCreatureSay(cid, type, msg)npcHandler:onCreatureSay(cid, type, msg:lower()) end
+function onThink() npcHandler:onThink() end
+function onThink() npcHandler:onThinkCreatureSay() end
+
 local talkState = {}
 
 -- Itens para entregar ao npc
 -- {Id, Quantidade, Level}
 local craft_items = {
-    {id = 2671, amount = 6},
+    {id = 2671, amount = 6}
 }
 
 -- Id do item que o player receberá
 local reward_item_id = 2376
 
-function creatureSayCallback(cid, type, msg)
-   if(not npcHandler:isFocused(cid)) then
-      return false
-   end
- 
-   local talkUser = NPCHANDLER_CONVBEHAVIOR == CONVERSATION_DEFAULT and 0 or cid
+function onCreatureSay(cid, type, msg)
+    local talkUser = NPCHANDLER_CONVBEHAVIOR == CONVERSATION_DEFAULT and 0 or cid
+    msg = msg:lower()
 
-	if msgcontains(msg, "entregar") then
+    if not npcHandler:isFocused(cid) and getDistanceBetween(getThingPos(cid), getNpcPos()) < 5 then
+        if isInArray({"hi", "hello", "oi", "olá"}, msg) then
+            npcHandler:addFocus(cid)
+            selfSayMultiLanguage(
+                "phrase.",
+                "frase.",
+                cid
+            )
+            return true
+        end
+    end
+    
+    if isInArray({"bye", "goodbye", "tchau", "adeus"}, msg) and npcHandler:isFocused(cid) then
+        npcHandler:releaseFocus(cid)
+        selfSayMultiLanguage(
+            "Goodbye!",
+            "Adeus!",
+            cid
+        )
+        return true
+    end
+
+    if (not npcHandler:isFocused(cid)) then
+        return false
+    end
+
+    if msgcontains(msg, "entregar") then
         for item_count = 1, #craft_items do
             if getPlayerItemCount(cid, craft_items[item_count].id) < craft_items[item_count].amount then
-                selfSayMultiLanguage(
-                    "You do not have the needed items.",
-                    "Você não tem os items necessários",
-                    cid
-                )
+                selfSayMultiLanguage("You do not have the needed items.", "Você não tem os items necessários", cid)
                 return true
             end
         end
@@ -46,9 +67,9 @@ function creatureSayCallback(cid, type, msg)
             "Muito obrigado por colaborar. Aqui está sua recompensa!",
             cid
         )
-	end
+    end
     return true
 end
- 
+
 npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
 npcHandler:addModule(FocusModule:new())
