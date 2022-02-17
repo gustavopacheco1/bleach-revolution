efdcf12197671a5e1ff3f617cf67ff402b18ef7a
Gustavo <gustavopacheco61@yahoo.com>
2022-02-17 14:28:54 -0300

    add: npc tessai script

---
+++ data/npc/scripts/tessai.lua
@@ -0,0 +1,50 @@
+local keywordHandler = KeywordHandler:new()
+local npcHandler = NpcHandler:new(keywordHandler)
+NpcSystem.parseParameters(npcHandler)
+
+function onCreatureAppear(cid)npcHandler:onCreatureAppear(cid) end
+function onCreatureDisappear(cid)npcHandler:onCreatureDisappear(cid) end
+function onCreatureSay(cid, type, msg)npcHandler:onCreatureSay(cid, type, msg:lower()) end
+function onThink() npcHandler:onThink() end
+function onThink() npcHandler:onThinkCreatureSay() end
+
+local talkState = {}
+
+function onCreatureSay(cid, type, msg)
+    local talkUser = NPCHANDLER_CONVBEHAVIOR == CONVERSATION_DEFAULT and 0 or cid
+    msg = msg:lower()
+
+    if not npcHandler:isFocused(cid) and getDistanceBetween(getThingPos(cid), getNpcPos()) < 5 then
+        if isInArray({"hi", "hello", "oi", "olá"}, msg) then
+            npcHandler:addFocus(cid)
+            selfSayMultiLanguage(
+                "Do you want to be sent to the quest?",
+                "Você deseja ser levado até a quest?",
+                cid
+            )
+            return true
+        end
+    end
+    
+    if isInArray({"bye", "goodbye", "tchau", "adeus"}, msg) and npcHandler:isFocused(cid) then
+        npcHandler:releaseFocus(cid)
+        selfSayMultiLanguage(
+            "Goodbye!",
+            "Adeus!",
+            cid
+        )
+        return true
+    end
+
+    if (not npcHandler:isFocused(cid)) then
+        return false
+    end
+
+    if msg == "yes" then
+        doTeleportThing(cid, {x = 4422, y = 3672, z = 7})
+    end
+    return true
+end
+
+npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
+npcHandler:addModule(FocusModule:new())
