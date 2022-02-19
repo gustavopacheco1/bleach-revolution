dbec1b19aed6e6fb0c30dca5f741d0f92d85bdac
Gustavo <gustavopacheco61@yahoo.com>
2022-02-19 16:08:19 -0300

    add: urahara muted spell

---
+++ data/spells/scripts/special/muted.lua
@@ -0,0 +1,31 @@
+local spell = {
+    cooldown = 60,
+    duration = 3,
+    effect = 20,
+}
+
+local condition = createConditionObject(CONDITION_MUTED)
+setConditionParam(condition, CONDITION_PARAM_TICKS, spell.duration * 1000)
+
+function onCastSpell(cid, var)
+	if exhaustion.check(cid, "special") then
+		doPlayerSendCancel(cid, "Cooldown " .. exhaustion.get(cid, "special") .. "s")
+		return false
+	end
+
+    local target = getCreatureTarget(cid)
+
+    if not (isPlayer(target)) then
+        MultiLanguage.doPlayerSendCancel(
+            cid,
+            "You may only use this technique in players.",
+            "Você só pode utilizar esta técnica em players."
+        )
+        return false
+    end
+
+    doSendMagicEffect(getCreaturePosition(target), spell.effect)
+    doAddCondition(target, condition)
+	exhaustion.set(cid, "special" , spell.cooldown)
+	return true
+end
