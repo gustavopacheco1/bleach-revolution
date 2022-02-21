e0ed533ed30ec500a09aac8a8b44abc53f45c02b
Gustavo <gustavopacheco61@yahoo.com>
2022-02-20 21:43:48 -0300

    add: MultiLanguage doPlayerSendTextMessage

--- data/lib/multi_language.lua
+++ data/lib/multi_language.lua
@@ -5,5 +5,13 @@ MultiLanguage = {
         end
 
         return doPlayerSendCancel(cid, message_pt)
+    end,
+
+    doPlayerSendTextMessage = function (cid, MessageClass, message_en, message_pt)
+        if getPlayerStorageValue(cid, "language") == "en" then
+            return doPlayerSendTextMessage(cid, MessageClass, message_en)
+        end
+
+        return doPlayerSendTextMessage(cid, MessageClass, message_pt)
     end
 }
