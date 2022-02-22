265b665c628ae7653c458210590428e89900389e
magojackson <97809271+magojackson@users.noreply.github.com>
2022-02-22 15:04:28 -0300

    Magia Fast do urahra mudado

--- data/spells/scripts/attack/Urahara/Benihime.lua
+++ data/spells/scripts/attack/Urahara/Benihime.lua
@@ -1,7 +1,7 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_HITCOLOR, COLOR_PINK)
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
-setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, 77)
+setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, 79)
 setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -15.0, 0, -15.0, 0)
 
 function onCastSpell(cid, var)
@@ -15,10 +15,10 @@ function onCastSpell(cid, var)
             if isCreature(cid) then
 				local target_position = getCreaturePosition(getCreatureTarget(cid))
                 doSendMagicEffect({
-                    x = target_position.x,
-                    y = target_position.y,
+                    x = target_position.x + 1,
+                    y = target_position.y + 1,
                     z = target_position.z
-                }, 700)
+                }, 704)
 
 				doCombat(cid, combat, var)
             end
