e1bae229e29a852429178f863cece05b3ce36b8b
magojackson <97809271+magojackson@users.noreply.github.com>
2022-02-22 15:15:04 -0300

    urahara atualizado

--- data/spells/scripts/attack/Urahara/Kamisori.lua
+++ data/spells/scripts/attack/Urahara/Kamisori.lua
@@ -1,20 +1,30 @@
 local combat = createCombatObject()
+setCombatParam(combat, COMBAT_PARAM_HITCOLOR, COLOR_PINK)
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
+setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, 80)
 setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -60.0, 0, -60.0, 0)
 
 function onCastSpell(cid, var)
-    if exhaustion.check(cid, 115818) then
+    if exhaustion.check(cid, 45670) then
         doPlayerSendCancel(cid, "You are exhausted.")
         return false
     end
 
-    local target_position = getCreaturePosition(getCreatureTarget(cid))
-    doSendMagicEffect({
-        x = target_position.x,
-        y = target_position.y,
-        z = target_position.z
-    }, 701)
-    
-    exhaustion.set(cid, 115818, 1)
-    return doCombat(cid, combat, var)
+    for i = 1, 1 do
+        addEvent(function()
+            if isCreature(cid) then
+				local target_position = getCreaturePosition(getCreatureTarget(cid))
+                doSendMagicEffect({
+                    x = target_position.x + 1,
+                    y = target_position.y + 1,
+                    z = target_position.z
+                }, 706)
+
+				doCombat(cid, combat, var)
+            end
+        end, i * 275)
+    end
+	
+    exhaustion.set(cid, 45670, 1)
+    return true
 end
