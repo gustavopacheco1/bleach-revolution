1ef86f82dc807245c3c3bd3d17d54330241a5d42
magojackson <97809271+magojackson@users.noreply.github.com>
2022-02-22 23:38:00 -0300

    fix debug por causa das armas

--- data/weapons/scripts/legendary del relampago.lua
+++ data/weapons/scripts/legendary del relampago.lua
@@ -1,6 +1,6 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
-setCombatParam(combat, COMBAT_PARAM_EFFECT, 6)
+setCombatParam(combat, COMBAT_PARAM_EFFECT, 486)
 setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, 64)
 setCombatFormula(combat, COMBAT_FORMULA_SKILL, 1, 0, 1, 0)
 
