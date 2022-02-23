fdd2c631b926fa3229a2b1c80db4c07270e2cb31
magojackson <97809271+magojackson@users.noreply.github.com>
2022-02-23 10:52:12 -0300

    adicinado um missile e 2 effect, refeito o combo do byakuya

--- data/spells/scripts/attack/Byakuya/Hakusui.lua
+++ data/spells/scripts/attack/Byakuya/Hakusui.lua
@@ -10,10 +10,10 @@ function onCastSpell(cid, var)
 
     local target_position = getCreaturePosition(getCreatureTarget(cid))
     doSendMagicEffect({
-        x = target_position.x + 1,
-        y = target_position.y + 1,
+        x = target_position.x,
+        y = target_position.y,
         z = target_position.z
-    }, 451)
+    }, 644)
     
     exhaustion.set(cid, 115818, 1)
     return doCombat(cid, combat, var)
