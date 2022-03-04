245c489dc044903f79eede6c2130b85989e17e6e
Gustavo <gustavopacheco61@yahoo.com>
2022-03-04 14:11:20 -0300

    add: Hiroshi Boss Two teleport

--- data/creaturescripts/scripts/monsters_events/monster_teleport_ondeath.lua
+++ data/creaturescripts/scripts/monsters_events/monster_teleport_ondeath.lua
@@ -7,7 +7,8 @@ local bosses = {
     ["Zaraki Legendary"] = {teleport_to = {x = 2769, y = 3937, z = 4}},
     ["Arrancar Boss"] = {teleport_to = {x = 3651, y = 4230, z = 8}},
     ["Ryu Boss"] = {teleport_to = {x = 3934, y = 3816, z = 8}},
-    ["Hiroshi Boss"] = {teleport_to = {x = 2789, y = 3234, z = 6}}
+    ["Hiroshi Boss"] = {teleport_to = {x = 2789, y = 3234, z = 6}},
+    ["Hiroshi Boss Two"] = {teleport_to = {x = 2789, y = 3294, z = 6}}
 }
 
 function onDeath(cid, corpse, deathList)
