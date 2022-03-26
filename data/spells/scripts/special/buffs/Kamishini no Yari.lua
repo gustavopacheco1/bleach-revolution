c2a2a3f3fe2d4b254da039d734e0c3e583e84318
magojackson <97809271+magojackson@users.noreply.github.com>
2022-03-26 15:09:39 -0300

    feito o buff do gin

---
+++ data/spells/scripts/special/buffs/Kamishini no Yari.lua
@@ -0,0 +1,37 @@
+local spell = {
+    cooldown = 50,
+    duration = 30,
+    outfit = 320,
+    effect = 457
+}
+
+local combat = createCombatObject()
+setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, false)
+
+local condition = createConditionObject(CONDITION_ATTRIBUTES)
+setConditionParam(condition, CONDITION_PARAM_TICKS, spell.duration * 1000)
+setConditionParam(condition, CONDITION_PARAM_BUFF, true)
+setConditionParam(condition, CONDITION_PARAM_STAT_MAGICLEVEL, 10)
+setConditionParam(condition, CONDITION_PARAM_SKILL_FIST, 10)
+setConditionParam(condition, CONDITION_PARAM_SKILL_SWORD, 10)
+setConditionParam(condition, CONDITION_PARAM_SKILL_SHIELD, 5)
+setCombatCondition(combat, condition)
+
+function onCastSpell(cid, var)
+    if exhaustion.check(cid, "special") then
+        doPlayerSendCancel(cid, "Cooldown " .. exhaustion.get(cid, "special") .. "s")
+        return false
+    end
+
+    local player_position = getCreaturePosition(cid)
+    doSendMagicEffect({
+        x = player_position.x + 1,
+        y = player_position.y + 1,
+        z = player_position.z,
+    }, spell.effect)
+
+	doSetCreatureOutfit(cid, {lookType = spell.outfit}, spell.duration * 1000)
+	exhaustion.set(cid, "special", spell.cooldown)
+	doCombat(cid, combat, var)
+    return true
+end
