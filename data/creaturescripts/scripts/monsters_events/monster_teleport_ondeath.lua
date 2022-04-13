e605537ab57528f7922e754a62163b40dd0e2a00
magojackson <97809271+magojackson@users.noreply.github.com>
2022-04-13 16:36:41 -0300

    transforms arrumadas no xml

--- data/creaturescripts/scripts/monsters_events/monster_teleport_ondeath.lua
+++ data/creaturescripts/scripts/monsters_events/monster_teleport_ondeath.lua
@@ -1,6 +1,6 @@
 local bosses = {
     ["Samurai Boss"] = {teleport_to = {x = 3330, y = 3627, z = 8}},
-    ["Kensei Boss"] = {teleport_to = {x = 3889, y = 3414, z = 6}},
+    ["Kensei Boss"] = {teleport_to = {x = 3895, y = 3416, z = 6}},
     ["Especial Hollow Snake"] = {teleport_to = {x = 3228, y = 3207, z = 8}},
     ["Barragan Boss"] = {teleport_to = {x = 4284, y = 3417, z = 6}},
     ["Legendary Grand Fisher"] = {storage = "tessai_quest", teleport_to = {x = 3583, y = 3456, z = 8}},
--- data/lib/vocations.lua
+++ data/lib/vocations.lua
@@ -1,124 +1,75 @@
 VOCATION_REVERT = {
     ["Ichigo Kurosaki"] = {vocation = 1, looktype = 2, effect = {number = 10}},
-    ["Chad"] = {vocation = 7, looktype = 157, effect = {number = 10}},
-    ["Bazz-B"] = {vocation = 13, looktype = 148, effect = {number = 10}},
-    ["Orihime Inoue"] = {vocation = 19, looktype = 59, effect = {number = 10}},
-    ["Uryu Ishida"] = {vocation = 25, looktype = 34, effect = {number = 10}},
-    ["Ulquiorra Ciffer"] = {vocation = 31, looktype = 113, effect = {number = 10}},
-    ["Zaraki Kenpachi"] = {vocation = 37, looktype = 123, effect = {number = 10}},
-    ["Byakuya Kuchiki"] = {vocation = 43, looktype = 85, effect = {number = 10}},
-    ["Toshiro Histugaya"] = {vocation = 49, looktype = 73, effect = {number = 10}},
-    ["Rukia Kuchiki"] = {vocation = 55, looktype = 20, effect = {number = 10}},
-    ["Retsu Unohana"] = {vocation = 68, looktype = 139, effect = {number = 10}},
-    ["Renjii Abarai"] = {vocation = 73, looktype = 47, effect = {number = 10}},
-	["Gin Ichimaru"] = {vocation = 79, looktype = 316, effect = {number = 10}},
-	["Kuugo Ginjou"] = {vocation = 85, looktype = 326, effect = {number = 10}},
-	["Tier Halibel"] = {vocation = 91, looktype = 321, effect = {number = 10}},
-	["Urahara Kisuke"] = {vocation = 98, looktype = 331, effect = {number = 10}},
+    ["Chad"] = {vocation = 2, looktype = 157, effect = {number = 10}},
+    ["Bazz-B"] = {vocation = 3, looktype = 148, effect = {number = 10}},
+    ["Orihime Inoue"] = {vocation = 4, looktype = 59, effect = {number = 10}},
+    ["Uryu Ishida"] = {vocation = 5, looktype = 34, effect = {number = 10}},
+    ["Ulquiorra Ciffer"] = {vocation = 6, looktype = 113, effect = {number = 10}},
+    ["Zaraki Kenpachi"] = {vocation = 7, looktype = 53, effect = {number = 10}},
+    ["Byakuya Kuchiki"] = {vocation = 8, looktype = 85, effect = {number = 10}},
+    ["Toshiro Histugaya"] = {vocation = 9, looktype = 73, effect = {number = 10}},
+    ["Rukia Kuchiki"] = {vocation = 10, looktype = 20, effect = {number = 10}},
+	["Nelliel Odelschwanck"] = {vocation = 11, looktype = 97, effect = {number = 10}},
+    ["Retsu Unohana"] = {vocation = 12, looktype = 139, effect = {number = 10}},
+    ["Renjii Abarai"] = {vocation = 13, looktype = 47, effect = {number = 10}},
+	["Gin Ichimaru"] = {vocation = 14, looktype = 316, effect = {number = 10}},
+	["Kuugo Ginjou"] = {vocation = 15, looktype = 326, effect = {number = 10}},
+	["Tier Halibel"] = {vocation = 16, looktype = 322, effect = {number = 10}},
+	["Urahara Kisuke"] = {vocation = 17, looktype = 331, effect = {number = 10}},
 }
 
 VOCATION_TRANSFORM = {
     -- Ichigo Kurosaki
-    [1] = {level = 100, vocation = 2, looktype = 3, effect = {number = 10}},
-    [2] = {level = 200, vocation = 3, looktype = 4, effect = {number = 10}},
-    [3] = {level = 300, vocation = 4, looktype = 5, effect = {number = 10}},
-    [4] = {level = 400, vocation = 5, looktype = 6, effect = {number = 10}},
+    [1] = {level = 100, vocation = 1, looktype = 3, effect = {number = 10}},
 
     -- Chad
-    [7] = {level = 100, vocation = 8, looktype = 158, effect = {number = 10}},
-    [8] = {level = 200, vocation = 9, looktype = 159, effect = {number = 10}},
-    [9] = {level = 300, vocation = 10, looktype = 160, effect = {number = 10}},
-    [10] = {level = 400, vocation = 11, looktype = 161, effect = {number = 10}},
-
+    [2] = {level = 100, vocation = 2, looktype = 158, effect = {number = 10}},
+    
     -- Bazz-B
-    [13] = {level = 100, vocation = 14, looktype = 149, effect = {number = 10}},
-    [14] = {level = 200, vocation = 15, looktype = 150, effect = {number = 10}},
-    [15] = {level = 300, vocation = 16, looktype = 151, effect = {number = 10}},
-    [16] = {level = 400, vocation = 17, looktype = 152, effect = {number = 10}},  
+    [3] = {level = 100, vocation = 3, looktype = 151, effect = {number = 10}}, 
 
     -- Orihime Inoue
-    [19] = {level = 100, vocation = 20, looktype = 60, effect = {number = 10}},
-    [20] = {level = 200, vocation = 21, looktype = 61, effect = {number = 10}},
-    [21] = {level = 300, vocation = 22, looktype = 62, effect = {number = 10}},
-    [22] = {level = 400, vocation = 23, looktype = 63, effect = {number = 10}},
+    [4] = {level = 100, vocation = 4, looktype = 60, effect = {number = 10}},
 
     -- Uryu Ishida
-    [25] = {level = 100, vocation = 26, looktype = 35, effect = {number = 10}},
-    [26] = {level = 200, vocation = 27, looktype = 36, effect = {number = 10}},
-    [27] = {level = 300, vocation = 28, looktype = 37, effect = {number = 10}},
-    [28] = {level = 400, vocation = 29, looktype = 38, effect = {number = 10}},
+    [5] = {level = 100, vocation = 5, looktype = 38, effect = {number = 10}},
 
     -- Ulquiorra Ciffer
-    [31] = {level = 100, vocation = 32, looktype = 114, effect = {number = 10}},
-    [32] = {level = 200, vocation = 33, looktype = 115, effect = {number = 10}},
-    [33] = {level = 300, vocation = 34, looktype = 116, effect = {number = 10}},
-    [34] = {level = 400, vocation = 35, looktype = 117, effect = {number = 10}},
-
+    [6] = {level = 100, vocation = 6, looktype = 114, effect = {number = 10}},
+   
     -- Zaraki Kenpachi
-    [37] = {level = 100, vocation = 38, looktype = 311, effect = {number = 10}},
-    [38] = {level = 200, vocation = 39, looktype = 312, effect = {number = 10}},
-    [39] = {level = 300, vocation = 40, looktype = 314, effect = {number = 10}},
-    [40] = {level = 400, vocation = 41, looktype = 315, effect = {number = 10}},
+    [7] = {level = 100, vocation = 7, looktype = 315, effect = {number = 10}},
 
     -- Byakuya Kuchiki
-    [43] = {level = 100, vocation = 44, looktype = 86, effect = {number = 10}},
-    [44] = {level = 200, vocation = 45, looktype = 87, effect = {number = 10}},
-    [45] = {level = 300, vocation = 46, looktype = 88, effect = {number = 10}},
-    [46] = {level = 400, vocation = 47, looktype = 89, effect = {number = 10}},
+    [8] = {level = 100, vocation = 8, looktype = 87, effect = {number = 10}},
 
     -- Toshiro Histugaya
-    [49] = {level = 100, vocation = 50, looktype = 74, effect = {number = 10}},
-    [50] = {level = 200, vocation = 51, looktype = 75, effect = {number = 10}},
-    [51] = {level = 300, vocation = 52, looktype = 76, effect = {number = 10}},
-    [52] = {level = 400, vocation = 53, looktype = 77, effect = {number = 10}},
+    [9] = {level = 100, vocation = 9, looktype = 74, effect = {number = 10}},
 
     -- Rukia Kuchiki
-    [55] = {level = 100, vocation = 56, looktype = 21, effect = {number = 10}},
-    [56] = {level = 200, vocation = 57, looktype = 22, effect = {number = 10}},
-    [57] = {level = 300, vocation = 58, looktype = 23, effect = {number = 10}},
-    [58] = {level = 400, vocation = 59, looktype = 24, effect = {number = 10}},
+    [10] = {level = 100, vocation = 10, looktype = 21, effect = {number = 10}},
 
     -- Nelliel Odelschwanck
-    [61] = {level = 100, vocation = 62, looktype = 98, effect = {number = 10}},
-    [62] = {level = 200, vocation = 63, looktype = 99, effect = {number = 10}},
-    [63] = {level = 300, vocation = 64, looktype = 100, effect = {number = 10}},
-    [64] = {level = 400, vocation = 65, looktype = 101, effect = {number = 10}},
+    [11] = {level = 100, vocation = 11, looktype = 99, effect = {number = 10}},
 
     -- Retsu Unohana
-    [67] = {level = 100, vocation = 68, looktype = 140, effect = {number = 10}},
-    [68] = {level = 200, vocation = 69, looktype = 141, effect = {number = 10}},
-    [69] = {level = 300, vocation = 70, looktype = 142, effect = {number = 10}},
-    [70] = {level = 400, vocation = 71, looktype = 143, effect = {number = 10}},
+    [12] = {level = 100, vocation = 12, looktype = 141, effect = {number = 10}},
 
     -- Renjii Abarai
-    [73] = {level = 100, vocation = 74, looktype = 48, effect = {number = 10}},
-    [74] = {level = 200, vocation = 75, looktype = 49, effect = {number = 10}},
-    [75] = {level = 300, vocation = 76, looktype = 50, effect = {number = 10}},
-    [76] = {level = 400, vocation = 77, looktype = 51, effect = {number = 10}},	
+    [13] = {level = 100, vocation = 13, looktype = 50, effect = {number = 10}},
 	
 	-- Gin Ichimaru 
-	[79] = {level = 100, vocation = 80, looktype = 317, effect = {number = 10}},
-    [80] = {level = 200, vocation = 81, looktype = 318, effect = {number = 10}},
-    [81] = {level = 300, vocation = 82, looktype = 319, effect = {number = 10}},
-    [82] = {level = 400, vocation = 83, looktype = 320, effect = {number = 10}},	
+	[14] = {level = 100, vocation = 14, looktype = 318, effect = {number = 10}},
 	
 	-- Kuugo Ginjou
-	[85] = {level = 100, vocation = 86, looktype = 327, effect = {number = 10}},
-    [86] = {level = 200, vocation = 87, looktype = 328, effect = {number = 10}},
-    [87] = {level = 300, vocation = 88, looktype = 329, effect = {number = 10}},
-    [88] = {level = 400, vocation = 89, looktype = 330, effect = {number = 10}},	
+	[15] = {level = 100, vocation = 15, looktype = 328, effect = {number = 10}},	
 	
 	-- Tier Halibel
-	[91] = {level = 100, vocation = 92, looktype = 322, effect = {number = 10}},
-    [92] = {level = 200, vocation = 93, looktype = 323, effect = {number = 10}},
-    [93] = {level = 300, vocation = 94, looktype = 324, effect = {number = 10}},
-    [94] = {level = 400, vocation = 95, looktype = 325, effect = {number = 10}},	
+	[16] = {level = 100, vocation = 16, looktype = 324, effect = {number = 10}},	
 	
 	-- Urahara Kisuke
-	[98] = {level = 100, vocation = 99, looktype = 332, effect = {number = 10}},
-    [99] = {level = 200, vocation = 100, looktype = 333, effect = {number = 10}},
-    [100] = {level = 300, vocation = 101, looktype = 334, effect = {number = 10}},
-    [101] = {level = 400, vocation = 102, looktype = 335, effect = {number = 10}},
+	[16] = {level = 100, vocation = 99, looktype = 333, effect = {number = 10}},
+    
 }
 
 vocation_specials = {
