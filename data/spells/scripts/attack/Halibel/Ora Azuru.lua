e6258720731b4f0079f9ab22b1e84d44f298bf6d
magojackson <97809271+magojackson@users.noreply.github.com>
2022-04-13 23:24:10 -0300

    fix spells ora azuru

--- data/spells/scripts/attack/Halibel/Ora Azuru.lua
+++ data/spells/scripts/attack/Halibel/Ora Azuru.lua
@@ -2,7 +2,7 @@ local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_HITCOLOR, COLOR_PINK)
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
 setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, 74)
-setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -11.0, 0, -11.0, 0)
+setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -16.0, 0, -16.0, 0)
 
 function onCastSpell(cid, var)
     if exhaustion.check(cid, 45670) then
@@ -13,19 +13,25 @@ function onCastSpell(cid, var)
     for i = 0, 4 do
         addEvent(function()
             if isCreature(cid) then
-				local target_position = getCreaturePosition(getCreatureTarget(cid))
-                doSendMagicEffect({
-                    x = target_position.x + 1,
-                    y = target_position.y + 1,
-                    z = target_position.z
-                }, 460)
+				local target = getCreatureTarget(cid)
 
+                if isCreature(target) then
+                    local target_position = getCreaturePosition(target)
 
-				doCombat(cid, combat, var)
+                    if isSightClear(getCreaturePosition(cid), target_position, false) then
+                        doSendMagicEffect({
+                            x = target_position.x + 1,
+                            y = target_position.y + 1,
+                            z = target_position.z
+                        }, 460)
+
+                        doCombat(cid, combat, numberToVariant(target))
+                    end
+                end
             end
         end, i * 275)
     end
-	
+
     exhaustion.set(cid, 45670, 1)
     return true
 end
