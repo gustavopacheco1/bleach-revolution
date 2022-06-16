bf61dc547cd49f761e11bc86f73054f320a5fb10
magojackson <97809271+magojackson@users.noreply.github.com>
2022-06-15 21:45:38 -0300

    feito o combo do Ikkaku.

---
+++ data/spells/scripts/attack/Ikkaku/Hozukimaru.lua
@@ -0,0 +1,37 @@
+local combat = createCombatObject()
+setCombatParam(combat, COMBAT_PARAM_HITCOLOR, COLOR_PINK)
+setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
+setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, 24)
+setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -16.0, 0, -16.0, 0)
+
+function onCastSpell(cid, var)
+    if exhaustion.check(cid, 45670) then
+        doPlayerSendCancel(cid, "You are exhausted.")
+        return false
+    end
+
+    for i = 0, 4 do
+        addEvent(function()
+            if isCreature(cid) then
+				local target = getCreatureTarget(cid)
+
+                if isCreature(target) then
+                    local target_position = getCreaturePosition(target)
+
+                    if isSightClear(getCreaturePosition(cid), target_position, false) then
+                        doSendMagicEffect({
+                            x = target_position.x + 1,
+                            y = target_position.y + 1,
+                            z = target_position.z
+                        }, 732)
+
+                        doCombat(cid, combat, numberToVariant(target))
+                    end
+                end
+            end
+        end, i * 275)
+    end
+
+    exhaustion.set(cid, 45670, 1)
+    return true
+end
