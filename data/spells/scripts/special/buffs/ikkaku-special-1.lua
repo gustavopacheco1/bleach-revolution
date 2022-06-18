18bf322f1768d838d83f96d742598e095a023d93
Gustavo <gustavopacheco61@yahoo.com>
2022-06-18 14:36:39 -0300

    Add especiais do Ikkaku

---
+++ data/spells/scripts/special/buffs/ikkaku-special-1.lua
@@ -0,0 +1,32 @@
+local spell = {
+    cooldown = 50,
+    duration = 30,
+    effect = 3
+}
+
+local combat = createCombatObject()
+setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, false)
+
+local condition = createConditionObject(CONDITION_ATTRIBUTES)
+setConditionParam(condition, CONDITION_PARAM_TICKS, spell.duration * 1000)
+setConditionParam(condition, CONDITION_PARAM_BUFF, true)
+setConditionParam(condition, CONDITION_PARAM_STAT_MAGICLEVEL, 15)
+setConditionParam(condition, CONDITION_PARAM_SKILL_FIST, 20)
+setCombatCondition(combat, condition)
+
+function onCastSpell(cid, var)
+    if checkSpecialCooldown(cid) then
+        return false
+    end
+
+    local player_position = getCreaturePosition(cid)
+    doSendMagicEffect({
+        x = player_position.x + 1,
+        y = player_position.y + 1,
+        z = player_position.z,
+    }, spell.effect)
+
+	exhaustion.set(cid, "special", spell.cooldown)
+	doCombat(cid, combat, var)
+    return true
+end
