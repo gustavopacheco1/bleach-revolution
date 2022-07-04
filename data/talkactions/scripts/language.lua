dcdf51920e36389b380c0b333b97a4c08e0ace96
Gustavo <gustavopacheco61@yahoo.com>
2022-07-04 10:37:51 -0300

    Refatora talkaction !language

--- data/talkactions/scripts/language.lua
+++ data/talkactions/scripts/language.lua
@@ -3,21 +3,18 @@ local english_parameters = { "en", "eng", "ingles", "inglês" }
 
 function onSay(cid, words, param, channel)
 	if isInArray(portuguese_parameters, param) then
-		setPlayerStorageValue(cid, "language", "pt")
+		doCreatureSetStorage(cid, "language", "pt")
 		doPlayerSendTextMessage(cid, MESSAGE_EVENT_ADVANCE, "O idioma do seu jogo foi trocado para o português.")
-	elseif isInArray(english_parameters, param) then
-		setPlayerStorageValue(cid, "language", "en")
-		doPlayerSendTextMessage(cid, MESSAGE_EVENT_ADVANCE, "Your game language was set to english.")
-	else
-		if getPlayerStorageValue(cid, "language") == -1 then
-			doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "[PT-BR] Escolha entre inglês e português")
-			doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "[EN] Choose between english and portuguese.")
-		elseif getPlayerStorageValue(cid, "language") == "pt" then
-			doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Escolha entre inglês e português")
-		else
-			doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Choose between english and portuguese.")
-		end
+		return true
 	end
 
+	if isInArray(english_parameters, param) then
+		doCreatureSetStorage(cid, "language", "en")
+		doPlayerSendTextMessage(cid, MESSAGE_EVENT_ADVANCE, "Your game language was set to english.")
+		return true
+	end
+
+	doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "[PT-BR] Escolha entre inglês e português")
+	doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "[EN] Choose between english and portuguese.")
 	return true
 end
