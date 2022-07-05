93e2551ef0fd7a394f340bdd87906458bd582686
magojackson <97809271+magojackson@users.noreply.github.com>
2022-07-05 00:13:25 -0300

    testeee

--- data/spells/scripts/attack/Ichigo/Getsuga Tenshou Final.lua
+++ data/spells/scripts/attack/Ichigo/Getsuga Tenshou Final.lua
@@ -3,7 +3,7 @@ setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
 setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -25.0, 0, -25.0, 0)
 
 function onCastSpell(cid, var)
-	if exhaustion.check(cid, 350) then
+	if exhaustion.check(cid, 300) then
 		doPlayerSendCancel(cid, "You are exhausted.")
 		return false
 	end
@@ -15,6 +15,6 @@ function onCastSpell(cid, var)
 		z = target_position.z
 	}, 15)
 
-	exhaustion.set(cid, 350, 3,5)
+	exhaustion.set(cid, 300, 3)
 	return doCombat(cid, combat, var)
 end
