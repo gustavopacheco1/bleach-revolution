cb8e0cf3f9839f98bd2c218802ea01749f3d0957
Gustavo <gustavopacheco61@yahoo.com>
2022-07-05 21:02:34 -0300

    Fix kamisori spell error

--- data/spells/scripts/attack/Urahara/Kamisori.lua
+++ data/spells/scripts/attack/Urahara/Kamisori.lua
@@ -2,7 +2,7 @@ local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_HITCOLOR, COLOR_PINK)
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
 setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, 80)
-setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -106.0, 0, -106,0, 0)
+setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -106.0, 0, -106, 0)
 
 function onCastSpell(cid, var)
 	if exhaustion.check(cid, 250) then
@@ -10,21 +10,15 @@ function onCastSpell(cid, var)
 		return false
 	end
 
-	for i = 1, 1 do
-		addEvent(function()
-			if isCreature(cid) then
-				local target_position = getCreaturePosition(getCreatureTarget(cid))
-				doSendMagicEffect({
-					x = target_position.x + 1,
-					y = target_position.y + 1,
-					z = target_position.z
-				}, 706)
+	local target_position = getCreaturePosition(getCreatureTarget(cid))
+	doSendMagicEffect({
+		x = target_position.x + 1,
+		y = target_position.y + 1,
+		z = target_position.z
+	}, 706)
 
-				doCombat(cid, combat, var)
-			end
-		end, i * 100)
-	end
+	doCombat(cid, combat, var)
 
-	exhaustion.set(cid, 250, 2,5)
+	exhaustion.set(cid, 250, 2)
 	return true
 end
