f4379dab7cee137e6b61cbe5dbc33e6b41b3ce2f
Gustavo <gustavopacheco61@yahoo.com>
2022-07-07 19:54:12 -0300

    Add storages e posição de efeito nos teleports

--- data/actions/scripts/teleports/teleports.lua
+++ data/actions/scripts/teleports/teleports.lua
@@ -3,10 +3,17 @@ local teleports = {
 	[20001] = { position = { x = 3434, y = 3585, z = 8 }, level = 100 },
 	[20002] = { position = { x = 2434, y = 3391, z = 7 }, level = 350 },
 	[20003] = { position = { x = 2433, y = 3395, z = 7 }, level = 350 },
+	[20004] = { position = { x = 3203, y = 2812, z = 15 }, storage = { key = "menus_florest_access", value = 1 }, effect = {id = 111, x = 1, y = 0} },
 }
 
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local teleport = teleports[getItemAttribute(item.uid, "uid")]
+
+	if teleport.storage then
+		if getCreatureStorage(cid, teleport.storage.key) ~= teleport.storage.value then
+			return false
+		end
+	end
 
 	if teleport.level then
 		if getPlayerLevel(cid) < teleport.level then
@@ -19,9 +26,14 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 		end
 	end
 
-	if teleport.effect then
-		doSendMagicEffect(teleport.position, teleport.effect)
-	end
 	doTeleportThing(cid, teleport.position)
+
+	if teleport.effect then
+		doSendMagicEffect({
+			x = teleport.position.x + teleport.effect.x,
+			y = teleport.position.y + teleport.effect.y,
+			z = teleport.position.z
+		}, teleport.effect.id)
+	end
 	return true
 end
