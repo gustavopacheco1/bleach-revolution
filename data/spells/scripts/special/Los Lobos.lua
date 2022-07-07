bdb7b056f6b5cd481f499b30d0773d6aaa806ced
magojackson <97809271+magojackson@users.noreply.github.com>
2022-07-07 16:59:34 -0300

    nome dos especiais das voc alterado.

---
+++ data/spells/scripts/special/Los Lobos.lua
@@ -0,0 +1,46 @@
+local combat = createCombatObject()
+setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, 76)
+
+function onCastSpell(cid, var)
+	if checkSpecialCooldown(cid) then
+		return false
+	end
+
+	setPlayerStorageValue(cid, "special", 1)
+	local player_base_speed = getCreatureBaseSpeed(cid)
+
+	local in_pz = false
+	for i = 0, 4 do
+		addEvent(function()
+			if not isCreature(cid) or in_pz then
+				return
+			end
+
+			if getTileInfo(getCreaturePosition(cid)).protection then
+				in_pz = true
+				doRemoveCreatureSummons(cid)
+
+				return MultiLanguage.doPlayerSendTextMessage(
+					cid,
+					MESSAGE_INFO_DESCR,
+					"Your technique was cancelled. You may not use it in protection zone.",
+					"Sua técnica foi cancelada. Você não pode utilizá-la em área protegida."
+				)
+			end
+
+			doSummonMonster(cid, "Wolf Clone")
+			for _, summon in ipairs(getCreatureSummons(cid)) do
+				if getCreatureSpeed(summon) ~= player_base_speed then
+					doChangeSpeed(summon, player_base_speed)
+				end
+				doCreatureSetHideHealth(summon, true)
+			end
+
+			if i == 4 then
+				registerCreatureEvent(cid, "WolfAttack")
+			end
+		end, i * 2000)
+	end
+
+	return true
+end
---
+++ data/spells/scripts/special/buffs/Lilynette.lua
@@ -0,0 +1,34 @@
+local spell = {
+	cooldown = 50,
+	duration = 30,
+	outfit = 451,
+	effect = 3
+}
+
+local combat = createCombatObject()
+setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, false)
+
+local condition = createConditionObject(CONDITION_ATTRIBUTES)
+setConditionParam(condition, CONDITION_PARAM_TICKS, spell.duration * 1000)
+setConditionParam(condition, CONDITION_PARAM_BUFF, true)
+setConditionParam(condition, CONDITION_PARAM_STAT_MAGICLEVEL, 15)
+setConditionParam(condition, CONDITION_PARAM_SKILL_FIST, 30)
+setCombatCondition(combat, condition)
+
+function onCastSpell(cid, var)
+	if checkSpecialCooldown(cid) then
+		return false
+	end
+
+	local player_position = getCreaturePosition(cid)
+	doSendMagicEffect({
+		x = player_position.x + 1,
+		y = player_position.y + 1,
+		z = player_position.z,
+	}, spell.effect)
+
+	doSetCreatureOutfit(cid, { lookType = spell.outfit }, spell.duration * 1000)
+	exhaustion.set(cid, "special", spell.cooldown)
+	doCombat(cid, combat, var)
+	return true
+end
