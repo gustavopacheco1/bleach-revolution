a8ddc2a50f42dd6f0e42445ec09f163bafb175c3
Gustavo <gustavopacheco61@yahoo.com>
2022-07-10 14:04:09 -0300

    Refatora potions script

--- data/actions/scripts/potions.lua
+++ data/actions/scripts/potions.lua
@@ -1,24 +1,24 @@
 local potions = {
 	-- great reiatsu potion
-	[16111] = { heal_mana = 30000, randomizer = 2500, effect = 502 },
+	[16111] = { heal = {mana = 30000, randomizer = 2500}, effect = 502 },
 
 	-- ultimate potion
-	[16112] = { heal_health = 15000, heal_mana = 15000, randomizer = 2500, effect = 502 },
+	[16112] = { heal = {health = 15000, mana = 15000, randomizer = 2500}, effect = 502 },
 
 	-- reiatsu potion
-	[16113] = { heal_mana = 15000, randomizer = 2500, effect = 502 },
+	[16113] = { heal = {mana = 15000, randomizer = 2500}, effect = 502 },
 
 	-- health potion
-	[16114] = { heal_health = 15000, randomizer = 2500, effect = 502 },
+	[16114] = { heal = {health = 15000, randomizer = 2500}, effect = 502 },
 
 	-- reiatsu pill
-	[16116] = { heal_mana = 8000, randomizer = 2500, effect = 502 },
+	[16116] = { heal = {mana = 8000, randomizer = 2500}, effect = 502 },
 
 	-- health pill
-	[16117] = { heal_health = 8000, randomizer = 2500, effect = 502 },
+	[16117] = { heal = {health = 8000, randomizer = 2500}, effect = 502 },
 
 	-- great health potion
-	[16118] = { heal_health = 30000, randomizer = 2500, effect = 502 }
+	[16118] = { heal = {health = 30000, randomizer = 2500}, effect = 502 }
 }
 
 local exhaust = createConditionObject(CONDITION_EXHAUST)
@@ -28,13 +28,12 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 	if isPlayer(itemEx.uid) and not hasCondition(cid, CONDITION_EXHAUST) then
 		local potion = potions[item.itemid]
 
-		if potion.heal_health then
-			doCreatureAddHealth(itemEx.uid,
-				math.random(potion.heal_health - potion.randomizer, potion.heal_health + potion.randomizer))
+		if potion.heal.health then
+			doCreatureAddHealth(itemEx.uid, math.random(potion.heal.health - potion.heal.randomizer, potion.heal.health + potion.heal.randomizer))
 		end
 
-		if potion.mana then
-			doCreatureAddMana(itemEx.uid, math.random(potion.heal_mana - potion.randomizer, potion.heal_mana + potion.randomizer))
+		if potion.heal.mana then
+			doCreatureAddMana(itemEx.uid, math.random(potion.heal.mana - potion.heal.randomizer, potion.heal.mana + potion.heal.randomizer))
 		end
 
 		doSendMagicEffect(toPosition, potions[item.itemid].effect)
