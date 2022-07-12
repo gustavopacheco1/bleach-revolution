1022518377a9ab9276ce0d1deb63f6d7bdce3c25
magojackson <97809271+magojackson@users.noreply.github.com>
2022-07-12 14:45:22 -0300

    atualizado weapon_soul_upgrade.lua/items.otb/items.xml

--- data/actions/scripts/weapon_soul_upgrade.lua
+++ data/actions/scripts/weapon_soul_upgrade.lua
@@ -1,15 +1,107 @@
 local weapons = {
-	["Zangetsu"] = { max_souls = 1000, upgrade_to = 15122 },
-	["Bankai Zangetsu"] = { max_souls = 2000, upgrade_to = 15411 },
-	["Legendary Bankai Zangetsu"] = { max_souls = 5000, upgrade_to = 15412 },
-
-	["El Diablo Glove"] = { max_souls = 1000, upgrade_to = 15819 },
-	["Brazo El Diablo"] = { max_souls = 2000, upgrade_to = 15431 },
-	["Legendary Brazo El Diablo"] = { max_souls = 5000, upgrade_to = 15432 },
-
-	["Quincy Bow"] = { max_souls = 1000, upgrade_to = 15823 },
-	["Lanza Del Relampago"] = { max_souls = 2000, upgrade_to = 15423 },
-	["Legendary Lanza Del Relampago"] = { max_souls = 5000, upgrade_to = 15424 }
+	["Zangetsu"] = { max_souls = 400, upgrade_to = 16340 },
+	["Zangetsu"] = { max_souls = 1000, upgrade_to = 16341 },
+	["Zangetsu"] = { max_souls = 2600, upgrade_to = 16342 },
+	["Zangetsu"] = { max_souls = 5000, upgrade_to = 16343 },
+	["Zangetsu"] = { max_souls = 12000, upgrade_to = 16344 },
+	
+	["Sode no Shirayuki"] = { max_souls = 400, upgrade_to = 16346 },
+	["Sode no Shirayuki"] = { max_souls = 1000, upgrade_to = 16347 },
+	["Sode no Shirayuki"] = { max_souls = 2600, upgrade_to = 16348 },
+	["Sode no Shirayuki"] = { max_souls = 5000, upgrade_to = 16349 },
+	["Sode no Shirayuki"] = { max_souls = 12000, upgrade_to = 16350 },
+	
+	["Zabimaru"] = { max_souls = 400, upgrade_to = 16352 },
+	["Zabimaru"] = { max_souls = 1000, upgrade_to = 16353 },
+	["Zabimaru"] = { max_souls = 2600, upgrade_to = 16354 },
+	["Zabimaru"] = { max_souls = 5000, upgrade_to = 16355 },
+	["Zabimaru"] = { max_souls = 12000, upgrade_to = 16356 },
+	
+	["Hyourinmaru"] = { max_souls = 400, upgrade_to = 16401 },
+	["Hyourinmaru"] = { max_souls = 1000, upgrade_to = 16408 },
+	["Hyourinmaru"] = { max_souls = 2600, upgrade_to = 16414 },
+	["Hyourinmaru"] = { max_souls = 5000, upgrade_to = 16420 },
+	["Hyourinmaru"] = { max_souls = 12000, upgrade_to = 16424 },
+	
+	["Gamuza"] = { max_souls = 400, upgrade_to = 16402 },
+	["Gamuza"] = { max_souls = 1000, upgrade_to = 16406 },
+	["Gamuza"] = { max_souls = 2600, upgrade_to = 16413 },
+	["Gamuza"] = { max_souls = 5000, upgrade_to = 16419 },
+	["Gamuza"] = { max_souls = 12000, upgrade_to = 16426 },
+	
+	["Pantera"] = { max_souls = 400, upgrade_to = 16399 },
+	["Pantera"] = { max_souls = 1000, upgrade_to = 16410 },
+	["Pantera"] = { max_souls = 2600, upgrade_to = 16416 },
+	["Pantera"] = { max_souls = 5000, upgrade_to = 16421 },
+	["Pantera"] = { max_souls = 12000, upgrade_to = 16425 },
+	
+	["Minazuki"] = { max_souls = 400, upgrade_to = 16400 },
+	["Minazuki"] = { max_souls = 1000, upgrade_to = 16409 },
+	["Minazuki"] = { max_souls = 2600, upgrade_to = 16415 },
+	["Minazuki"] = { max_souls = 5000, upgrade_to = 16422 },
+	["Minazuki"] = { max_souls = 12000, upgrade_to = 16423 },
+	
+	["Nozarashi"] = { max_souls = 400, upgrade_to = 16358 },
+	["Nozarashi"] = { max_souls = 1000, upgrade_to = 16359 },
+	["Nozarashi"] = { max_souls = 2600, upgrade_to = 16360 },
+	["Nozarashi"] = { max_souls = 5000, upgrade_to = 16361 },
+	["Nozarashi"] = { max_souls = 12000, upgrade_to = 16362 },
+	
+	["Benihime"] = { max_souls = 400, upgrade_to = 16364 },
+	["Benihime"] = { max_souls = 1000, upgrade_to = 16365 },
+	["Benihime"] = { max_souls = 2600, upgrade_to = 16366 },
+	["Benihime"] = { max_souls = 5000, upgrade_to = 16367 },
+	["Benihime"] = { max_souls = 12000, upgrade_to = 16368 },
+	
+	["Houzukimaru"] = { max_souls = 400, upgrade_to = 16370 },
+	["Houzukimaru"] = { max_souls = 1000, upgrade_to = 16371 },
+	["Houzukimaru"] = { max_souls = 2600, upgrade_to = 16372 },
+	["Houzukimaru"] = { max_souls = 5000, upgrade_to = 16373 },
+	["Houzukimaru"] = { max_souls = 12000, upgrade_to = 16374 },
+	
+	["Senbonzakura"] = { max_souls = 400, upgrade_to = 16430 },
+	["Senbonzakura"] = { max_souls = 1000, upgrade_to = 16431 },
+	["Senbonzakura"] = { max_souls = 2600, upgrade_to = 16432 },
+	["Senbonzakura"] = { max_souls = 5000, upgrade_to = 16433 },
+	["Senbonzakura"] = { max_souls = 12000, upgrade_to = 16434 },
+	
+	["Santa Teresa"] = { max_souls = 400, upgrade_to = 16403 },
+	["Santa Teresa"] = { max_souls = 1000, upgrade_to = 16407 },
+	["Santa Teresa"] = { max_souls = 2600, upgrade_to = 16412 },
+	["Santa Teresa"] = { max_souls = 5000, upgrade_to = 16418 },
+	["Santa Teresa"] = { max_souls = 12000, upgrade_to = 16427 },
+	
+	
+	["Brazo Derecha de Gigante"] = { max_souls = 400, upgrade_to = 16376 },
+	["Brazo Derecha de Gigante"] = { max_souls = 1000, upgrade_to = 16377 },
+	["Brazo Derecha de Gigante"] = { max_souls = 2600, upgrade_to = 16378 },
+	["Brazo Derecha de Gigante"] = { max_souls = 5000, upgrade_to = 16379 },
+	["Brazo Derecha de Gigante"] = { max_souls = 12000, upgrade_to = 16380 },
+	
+	
+	["Kojaku"] = { max_souls = 400, upgrade_to = 16382 },
+	["Kojaku"] = { max_souls = 1000, upgrade_to = 16383 },
+	["Kojaku"] = { max_souls = 2600, upgrade_to = 16384 },
+	["Kojaku"] = { max_souls = 5000, upgrade_to = 16385 },
+	["Kojaku"] = { max_souls = 12000, upgrade_to = 16386 },
+	
+	["Shun Shun Rikka"] = { max_souls = 400, upgrade_to = 16388 },
+	["Shun Shun Rikka"] = { max_souls = 1000, upgrade_to = 16389 },
+	["Shun Shun Rikka"] = { max_souls = 2600, upgrade_to = 16390 },
+	["Shun Shun Rikka"] = { max_souls = 5000, upgrade_to = 16391 },
+	["Shun Shun Rikka"] = { max_souls = 12000, upgrade_to = 16392 },
+	
+	["Heiling Bogen"] = { max_souls = 400, upgrade_to = 16404 },
+	["Heiling Bogen"] = { max_souls = 1000, upgrade_to = 16405 },
+	["Heiling Bogen"] = { max_souls = 2600, upgrade_to = 16411 },
+	["Heiling Bogen"] = { max_souls = 5000, upgrade_to = 16417 },
+	["Heiling Bogen"] = { max_souls = 12000, upgrade_to = 16428 },
+	
+	["Lanza Del Relampago"] = { max_souls = 400, upgrade_to = 16394 },
+	["Lanza Del Relampago"] = { max_souls = 1000, upgrade_to = 16395 },
+	["Lanza Del Relampago"] = { max_souls = 2600, upgrade_to = 16396 },
+	["Lanza Del Relampago"] = { max_souls = 5000, upgrade_to = 16397 },
+	["Lanza Del Relampago"] = { max_souls = 12000, upgrade_to = 16398 }
 }
 
 local upgrade_items_id = {
