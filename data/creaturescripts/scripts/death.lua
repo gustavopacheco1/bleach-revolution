e74767f6d0be40e27e53f4457dd9669dd61c204f
magojackson <97809271+magojackson@users.noreply.github.com>
2022-07-21 14:04:30 -0300

    test death.lua

--- data/creaturescripts/scripts/death.lua
+++ data/creaturescripts/scripts/death.lua
@@ -27,7 +27,7 @@ function onStatsChange(cid, attacker, type, combat, value)
 
 	setCreatureTarget(cid, nil)
 	doCreatureAddHealth(cid, getCreatureMaxHealth(cid))
-	doPlayerSetGroupId(cid, 7)
+	doPlayerSetGroupId(cid, 14)
 	if not isPlayerGhost(cid) then
 		doCreatureExecuteTalkAction(cid, "###invisible", true)
 	end
