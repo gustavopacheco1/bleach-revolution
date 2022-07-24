763f6b89e920a8d2ad83c3bf43940eb144d58683
Gustavo <gustavopacheco61@yahoo.com>
2022-07-24 14:43:48 -0300

    Refatora weapon soul upgrade system - Faz funcionar com ids em vez de nome da arma

--- data/actions/scripts/weapon_soul_upgrade.lua
+++ data/actions/scripts/weapon_soul_upgrade.lua
@@ -1,116 +1,132 @@
 local weapons = {
-	["Zangetsu"] = { max_souls = 400, upgrade_to = 16340 },
-	["16340"] = { max_souls = 1000, upgrade_to = 16341 },
-	["16341"] = { max_souls = 2600, upgrade_to = 16342 },
-	["16342"] = { max_souls = 5000, upgrade_to = 16343 },
-	["16343"] = { max_souls = 12000, upgrade_to = 16344 },
-	
-	["Sode no Shirayuki"] = { max_souls = 400, upgrade_to = 16346 },
-	["16346"] = { max_souls = 1000, upgrade_to = 16347 },
-	["16347"] = { max_souls = 2600, upgrade_to = 16348 },
-	["16348"] = { max_souls = 5000, upgrade_to = 16349 },
-	["16349"] = { max_souls = 12000, upgrade_to = 16350 },
-	
-	["Zabimaru"] = { max_souls = 400, upgrade_to = 16352 },
-	["16352"] = { max_souls = 1000, upgrade_to = 16353 },
-	["16353"] = { max_souls = 2600, upgrade_to = 16354 },
-	["16354"] = { max_souls = 5000, upgrade_to = 16355 },
-	["16355"] = { max_souls = 12000, upgrade_to = 16356 },
-	
-	["Hyourinmaru"] = { max_souls = 400, upgrade_to = 16401 },
-	["16401"] = { max_souls = 1000, upgrade_to = 16408 },
-	["16408"] = { max_souls = 2600, upgrade_to = 16414 },
-	["16414"] = { max_souls = 5000, upgrade_to = 16420 },
-	["16420"] = { max_souls = 12000, upgrade_to = 16424 },
-	
-	["Gamuza"] = { max_souls = 400, upgrade_to = 16402 },
-	["16402"] = { max_souls = 1000, upgrade_to = 16406 },
-	["16406"] = { max_souls = 2600, upgrade_to = 16413 },
-	["16413"] = { max_souls = 5000, upgrade_to = 16419 },
-	["16419"] = { max_souls = 12000, upgrade_to = 16426 },
-	
-	["Pantera"] = { max_souls = 400, upgrade_to = 16399 },
-	["16399"] = { max_souls = 1000, upgrade_to = 16410 },
-	["16410"] = { max_souls = 2600, upgrade_to = 16416 },
-	["16416"] = { max_souls = 5000, upgrade_to = 16421 },
-	["16421"] = { max_souls = 12000, upgrade_to = 16425 },
-	
-	["Minazuki"] = { max_souls = 400, upgrade_to = 16400 },
-	["16400"] = { max_souls = 1000, upgrade_to = 16409 },
-	["16409"] = { max_souls = 2600, upgrade_to = 16415 },
-	["16415"] = { max_souls = 5000, upgrade_to = 16422 },
-	["16422"] = { max_souls = 12000, upgrade_to = 16423 },
-	
-	["Nozarashi"] = { max_souls = 400, upgrade_to = 16358 },
-	["16358"] = { max_souls = 1000, upgrade_to = 16359 },
-	["16359"] = { max_souls = 2600, upgrade_to = 16360 },
-	["16360"] = { max_souls = 5000, upgrade_to = 16361 },
-	["16361"] = { max_souls = 12000, upgrade_to = 16362 },
-	
-	["Benihime"] = { max_souls = 400, upgrade_to = 16364 },
-	["16364"] = { max_souls = 1000, upgrade_to = 16365 },
-	["16365"] = { max_souls = 2600, upgrade_to = 16366 },
-	["16366"] = { max_souls = 5000, upgrade_to = 16367 },
-	["16367"] = { max_souls = 12000, upgrade_to = 16368 },
-	
-	["Houzukimaru"] = { max_souls = 400, upgrade_to = 16370 },
-	["16370"] = { max_souls = 1000, upgrade_to = 16371 },
-	["16371"] = { max_souls = 2600, upgrade_to = 16372 },
-	["16372"] = { max_souls = 5000, upgrade_to = 16373 },
-	["16373"] = { max_souls = 12000, upgrade_to = 16374 },
-	
-	["Senbonzakura"] = { max_souls = 400, upgrade_to = 16430 },
-	["16430"] = { max_souls = 1000, upgrade_to = 16431 },
-	["16431"] = { max_souls = 2600, upgrade_to = 16432 },
-	["16432"] = { max_souls = 5000, upgrade_to = 16433 },
-	["16433"] = { max_souls = 12000, upgrade_to = 16434 },
-	
-	["Santa Teresa"] = { max_souls = 400, upgrade_to = 16403 },
-	["16403"] = { max_souls = 1000, upgrade_to = 16407 },
-	["16407"] = { max_souls = 2600, upgrade_to = 16412 },
-	["16412"] = { max_souls = 5000, upgrade_to = 16418 },
-	["16418"] = { max_souls = 12000, upgrade_to = 16427 },
-	
-	
-	["Brazo Derecha de Gigante"] = { max_souls = 400, upgrade_to = 16376 },
-	["16376"] = { max_souls = 1000, upgrade_to = 16377 },
-	["16377"] = { max_souls = 2600, upgrade_to = 16378 },
-	["16378"] = { max_souls = 5000, upgrade_to = 16379 },
-	["16379"] = { max_souls = 12000, upgrade_to = 16380 },
-	
-	
-	["Kojaku"] = { max_souls = 400, upgrade_to = 16382 },
-	["16382"] = { max_souls = 1000, upgrade_to = 16383 },
-	["16383"] = { max_souls = 2600, upgrade_to = 16384 },
-	["16384"] = { max_souls = 5000, upgrade_to = 16385 },
-	["16385"] = { max_souls = 12000, upgrade_to = 16386 },
-	
-	["Shun Shun Rikka"] = { max_souls = 400, upgrade_to = 16388 },
-	["16388"] = { max_souls = 1000, upgrade_to = 16389 },
-	["16389"] = { max_souls = 2600, upgrade_to = 16390 },
-	["16390"] = { max_souls = 5000, upgrade_to = 16391 },
-	["16391"] = { max_souls = 12000, upgrade_to = 16392 },
-	
-	["Heiling Bogen"] = { max_souls = 400, upgrade_to = 16404 },
-	["16404"] = { max_souls = 1000, upgrade_to = 16405 },
-	["16405"] = { max_souls = 2600, upgrade_to = 16411 },
-	["16411"] = { max_souls = 5000, upgrade_to = 16417 },
-	["16417"] = { max_souls = 12000, upgrade_to = 16428 },
-	
-	["Lanza Del Relampago"] = { max_souls = 400, upgrade_to = 16394 },
-	["16394"] = { max_souls = 1000, upgrade_to = 16395 },
-	["16395"] = { max_souls = 2600, upgrade_to = 16396 },
-	["16396"] = { max_souls = 5000, upgrade_to = 16397 },
-	["16397"] = { max_souls = 12000, upgrade_to = 16398 },
-	
-	["Lilynette Gingerbuck"] = { max_souls = 400, upgrade_to = 16447 },
-	["16447"] = { max_souls = 1000, upgrade_to = 16448 },
-	["16448"] = { max_souls = 2600, upgrade_to = 16449 },
-	["16449"] = { max_souls = 5000, upgrade_to = 16450 },
-	["16450"] = { max_souls = 12000, upgrade_to = 16451 }
+	-- Zangetsu
+	[16339] = { max_souls = 400, upgrade_to = 16340 },
+	[16340] = { max_souls = 1000, upgrade_to = 16341 },
+	[16341] = { max_souls = 2600, upgrade_to = 16342 },
+	[16342] = { max_souls = 5000, upgrade_to = 16343 },
+	[16343] = { max_souls = 12000, upgrade_to = 16344 },
+
+	-- Sode no Shirayuki
+	[16345] = { max_souls = 400, upgrade_to = 16346 },
+	[16346] = { max_souls = 1000, upgrade_to = 16347 },
+	[16347] = { max_souls = 2600, upgrade_to = 16348 },
+	[16348] = { max_souls = 5000, upgrade_to = 16349 },
+	[16349] = { max_souls = 12000, upgrade_to = 16350 },
+
+	-- Zabimaru
+	[16351] = { max_souls = 400, upgrade_to = 16352 },
+	[16352] = { max_souls = 1000, upgrade_to = 16353 },
+	[16353] = { max_souls = 2600, upgrade_to = 16354 },
+	[16354] = { max_souls = 5000, upgrade_to = 16355 },
+	[16355] = { max_souls = 12000, upgrade_to = 16356 },
+
+	-- Hyourinmaru
+	[15736] = { max_souls = 400, upgrade_to = 16401 },
+	[16401] = { max_souls = 1000, upgrade_to = 16408 },
+	[16408] = { max_souls = 2600, upgrade_to = 16414 },
+	[16414] = { max_souls = 5000, upgrade_to = 16420 },
+	[16420] = { max_souls = 12000, upgrade_to = 16424 },
+
+	-- Gamuza
+	[16336] = { max_souls = 400, upgrade_to = 16402 },
+	[16402] = { max_souls = 1000, upgrade_to = 16406 },
+	[16406] = { max_souls = 2600, upgrade_to = 16413 },
+	[16413] = { max_souls = 5000, upgrade_to = 16419 },
+	[16419] = { max_souls = 12000, upgrade_to = 16426 },
+
+	-- Pantera
+	[16335] = { max_souls = 400, upgrade_to = 16399 },
+	[16399] = { max_souls = 1000, upgrade_to = 16410 },
+	[16410] = { max_souls = 2600, upgrade_to = 16416 },
+	[16416] = { max_souls = 5000, upgrade_to = 16421 },
+	[16421] = { max_souls = 12000, upgrade_to = 16425 },
+
+	-- Minazuki
+	[16338] = { max_souls = 400, upgrade_to = 16400 },
+	[16400] = { max_souls = 1000, upgrade_to = 16409 },
+	[16409] = { max_souls = 2600, upgrade_to = 16415 },
+	[16415] = { max_souls = 5000, upgrade_to = 16422 },
+	[16422] = { max_souls = 12000, upgrade_to = 16423 },
+
+	-- Nozarashi
+	[16357] = { max_souls = 400, upgrade_to = 16358 },
+	[16358] = { max_souls = 1000, upgrade_to = 16359 },
+	[16359] = { max_souls = 2600, upgrade_to = 16360 },
+	[16360] = { max_souls = 5000, upgrade_to = 16361 },
+	[16361] = { max_souls = 12000, upgrade_to = 16362 },
+
+	-- Benihime
+	[16363] = { max_souls = 400, upgrade_to = 16364 },
+	[16364] = { max_souls = 1000, upgrade_to = 16365 },
+	[16365] = { max_souls = 2600, upgrade_to = 16366 },
+	[16366] = { max_souls = 5000, upgrade_to = 16367 },
+	[16367] = { max_souls = 12000, upgrade_to = 16368 },
+
+	-- Houzukimaru
+	[16369] = { max_souls = 400, upgrade_to = 16370 },
+	[16370] = { max_souls = 1000, upgrade_to = 16371 },
+	[16371] = { max_souls = 2600, upgrade_to = 16372 },
+	[16372] = { max_souls = 5000, upgrade_to = 16373 },
+	[16373] = { max_souls = 12000, upgrade_to = 16374 },
+
+	-- Senbonzakura
+	[16429] = { max_souls = 400, upgrade_to = 16430 },
+	[16430] = { max_souls = 1000, upgrade_to = 16431 },
+	[16431] = { max_souls = 2600, upgrade_to = 16432 },
+	[16432] = { max_souls = 5000, upgrade_to = 16433 },
+	[16433] = { max_souls = 12000, upgrade_to = 16434 },
+
+	-- Santa Teresa
+	[16337] = { max_souls = 400, upgrade_to = 16403 },
+	[16403] = { max_souls = 1000, upgrade_to = 16407 },
+	[16407] = { max_souls = 2600, upgrade_to = 16412 },
+	[16412] = { max_souls = 5000, upgrade_to = 16418 },
+	[16418] = { max_souls = 12000, upgrade_to = 16427 },
+
+	-- Brazo Derecha de Gigante
+	[16375] = { max_souls = 400, upgrade_to = 16376 },
+	[16376] = { max_souls = 1000, upgrade_to = 16377 },
+	[16377] = { max_souls = 2600, upgrade_to = 16378 },
+	[16378] = { max_souls = 5000, upgrade_to = 16379 },
+	[16379] = { max_souls = 12000, upgrade_to = 16380 },
+
+	-- Kojaku
+	[16381] = { max_souls = 400, upgrade_to = 16382 },
+	[16382] = { max_souls = 1000, upgrade_to = 16383 },
+	[16383] = { max_souls = 2600, upgrade_to = 16384 },
+	[16384] = { max_souls = 5000, upgrade_to = 16385 },
+	[16385] = { max_souls = 12000, upgrade_to = 16386 },
+
+	-- Shun Shun Rikka
+	[16387] = { max_souls = 400, upgrade_to = 16388 },
+	[16388] = { max_souls = 1000, upgrade_to = 16389 },
+	[16389] = { max_souls = 2600, upgrade_to = 16390 },
+	[16390] = { max_souls = 5000, upgrade_to = 16391 },
+	[16391] = { max_souls = 12000, upgrade_to = 16392 },
+
+	-- Heiling Bogen
+	[16334] = { max_souls = 400, upgrade_to = 16404 },
+	[16404] = { max_souls = 1000, upgrade_to = 16405 },
+	[16405] = { max_souls = 2600, upgrade_to = 16411 },
+	[16411] = { max_souls = 5000, upgrade_to = 16417 },
+	[16417] = { max_souls = 12000, upgrade_to = 16428 },
+
+	-- Lanza Del Relampago
+	[16393] = { max_souls = 400, upgrade_to = 16394 },
+	[16394] = { max_souls = 1000, upgrade_to = 16395 },
+	[16395] = { max_souls = 2600, upgrade_to = 16396 },
+	[16396] = { max_souls = 5000, upgrade_to = 16397 },
+	[16397] = { max_souls = 12000, upgrade_to = 16398 },
+
+	-- Lilynette Gingerbuck
+	[16446] = { max_souls = 400, upgrade_to = 16447 },
+	[16447] = { max_souls = 1000, upgrade_to = 16448 },
+	[16448] = { max_souls = 2600, upgrade_to = 16449 },
+	[16449] = { max_souls = 5000, upgrade_to = 16450 },
+	[16450] = { max_souls = 12000, upgrade_to = 16451 }
 }
 
-local upgrade_items_id = {
+local upgrade_items = {
 	[10624] = { soul_upgrade = 20 },
 	[10625] = { soul_upgrade = 30 },
 	[10626] = { soul_upgrade = 40 },
@@ -118,15 +134,19 @@ local upgrade_items_id = {
 }
 
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if upgrade_items_id[item.itemid] then
-		if not weapons[getItemInfo(itemEx.itemid).name] then return end
+	-- Upgrade items
+	local upgrade_item = upgrade_items[item.itemid]
+
+	if upgrade_item then
+		local weapon = weapons[itemEx.itemid]
+
+		if not weapon then return false end
 
 		-- We need to set item attribute by function for using getItemAttribute
 		if getItemAttribute(itemEx.uid, "description") == nil then
 			doItemSetAttribute(itemEx.uid, "description", "Soul: 0")
 		end
 
-		local weapon = weapons[getItemInfo(itemEx.itemid).name]
 		local weapon_soul_amount = tonumber(string.explode(getItemAttribute(itemEx.uid, "description"), " ")[2])
 
 		if weapon_soul_amount >= weapon.max_souls then
@@ -138,11 +158,10 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 			return true
 		end
 
-		if (weapon_soul_amount + upgrade_items_id[item.itemid].soul_upgrade) > weapon.max_souls then
+		if (weapon_soul_amount + upgrade_item.soul_upgrade) > weapon.max_souls then
 			doItemSetAttribute(itemEx.uid, "description", "Soul: " .. tostring(weapon.max_souls))
 		else
-			doItemSetAttribute(itemEx.uid, "description",
-				"Soul: " .. tostring(weapon_soul_amount + upgrade_items_id[item.itemid].soul_upgrade))
+			doItemSetAttribute(itemEx.uid, "description", "Soul: " .. tostring(weapon_soul_amount + upgrade_items[item.itemid].soul_upgrade))
 		end
 
 		doSendMagicEffect(fromPosition, 13)
@@ -150,14 +169,16 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 		return true
 	end
 
-	if weapons[getItemInfo(item.itemid).name] then
-		-- ItemEx must have actionid 30000
+	-- Weapons
+	local weapon = weapons[item.itemid]
+
+	if weapon then
+		-- ItemEx must have uniqueid 30000
 		if not (getItemAttribute(itemEx.uid, "uid") == 30000) then return end
 
 		-- We need to set item attribute by function for using getItemAttribute
 		if getItemAttribute(item.uid, "description") == nil then return end
 
-		local weapon = weapons[getItemInfo(item.itemid).name]
 		local weapon_soul_amount = tonumber(string.explode(getItemAttribute(item.uid, "description"), " ")[2])
 
 		if weapon_soul_amount < weapon.max_souls then
@@ -175,7 +196,7 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 		}, 600)
 
 		doTransformItem(item.uid, weapon.upgrade_to)
-		if weapons[getItemInfo(weapon.upgrade_to).name] then
+		if weapons[weapon.upgrade_to] then
 			doItemSetAttribute(item.uid, "description", "Soul: 0")
 		else
 			doItemSetAttribute(item.uid, "description", "")
