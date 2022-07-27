d45801069315e5e49ad92891cde485e1b7a0bdc7
Gustavo <gustavopacheco61@yahoo.com>
2022-07-27 00:37:19 -0300

    Add `CustomSpell` functions

--- data/spells/lib/spells.lua
+++ data/spells/lib/spells.lua
@@ -30,6 +30,172 @@ function doCombatLoop(cid, combat, loop_amount, loop_ms_delay, effect)
 	end
 end
 
+CustomSpell = {
+	randomShoot = function(cid, combat, loop_amount, loop_ms_delay, missile, effect)
+		for i = 0, loop_amount - 1 do
+			addEvent(function()
+				if not isCreature(cid) then
+					return
+				end
+
+				local target = getCreatureTarget(cid)
+
+				if not isCreature(target) then
+					return
+				end
+
+				local target_position = getCreaturePosition(target)
+				local player_position = getCreaturePosition(cid)
+
+				if not isSightClear(player_position, target_position, false) then
+					return
+				end
+
+				for i = 1, 3 do
+					doSendDistanceShoot({
+						x = player_position.x + math.random(-1, 1),
+						y = player_position.y + math.random(-1, 1),
+						z = player_position.z
+					}, target_position, missile)
+				end
+
+				doSendMagicEffect({
+					x = target_position.x + effect.x,
+					y = target_position.y + effect.y,
+					z = target_position.z
+				}, effect.id)
+
+				doCombat(cid, combat, numberToVariant(target))
+			end, i * loop_ms_delay)
+		end
+	end,
+
+	randomEffect = function(cid, combat, loop_amount, loop_ms_delay, missile, effect)
+		for i = 0, loop_amount - 1 do
+			addEvent(function()
+				if not isCreature(cid) then
+					return
+				end
+
+				local target = getCreatureTarget(cid)
+
+				if not isCreature(target) then
+					return
+				end
+
+				local target_position = getCreaturePosition(target)
+				local player_position = getCreaturePosition(cid)
+
+				if not isSightClear(player_position, target_position, false) then
+					return
+				end
+
+				for i = 1, 3 do
+					doSendMagicEffect({
+						x = target_position.x + effect.x + math.random(-1, 1),
+						y = target_position.y + effect.y + math.random(-1, 1),
+						z = target_position.z
+					}, effect.id)
+				end
+
+				doSendDistanceShoot({
+					x = player_position.x,
+					y = player_position.y,
+					z = player_position.z
+				}, target_position, missile)
+
+				doCombat(cid, combat, numberToVariant(target))
+			end, i * loop_ms_delay)
+		end
+	end,
+
+	randomShootEffect = function(cid, combat, loop_amount, loop_ms_delay, missile, effect)
+		for i = 0, loop_amount - 1 do
+			addEvent(function()
+				if not isCreature(cid) then
+					return
+				end
+
+				local target = getCreatureTarget(cid)
+
+				if not isCreature(target) then
+					return
+				end
+
+				local target_position = getCreaturePosition(target)
+				local player_position = getCreaturePosition(cid)
+
+				if not isSightClear(player_position, target_position, false) then
+					return
+				end
+
+				for i = 1, 3 do
+					doSendMagicEffect({
+						x = target_position.x + effect.x + math.random(-1, 1),
+						y = target_position.y + effect.y + math.random(-1, 1),
+						z = target_position.z
+					}, effect.id)
+
+					doSendDistanceShoot({
+						x = player_position.x + math.random(-1, 1),
+						y = player_position.y + math.random(-1, 1),
+						z = player_position.z
+					}, target_position, missile)
+				end
+
+				doCombat(cid, combat, numberToVariant(target))
+			end, i * loop_ms_delay)
+		end
+	end,
+
+	skyShoot = function(cid, combat, loop_amount, loop_ms_delay, missile, effect)
+		for loop = 0, loop_amount - 1 do
+			addEvent(function()
+				if not isCreature(cid) then
+					return
+				end
+
+				local target = getCreatureTarget(cid)
+
+				if not isCreature(target) then
+					return
+				end
+
+				local target_position = getCreaturePosition(target)
+				local player_position = getCreaturePosition(cid)
+
+				if not isSightClear(player_position, target_position, false) then
+					return
+				end
+
+
+				for i = 1, 3 do
+					local rand = { x = math.random(-1, 1), y = math.random(-1, 1) }
+
+					doSendDistanceShoot({
+						x = target_position.x - 5 + rand.x,
+						y = target_position.y - 5 + rand.y,
+						z = target_position.z
+					}, {
+						x = target_position.x + rand.x,
+						y = target_position.y + rand.y,
+						z = target_position.z
+					}, missile)
+
+					doSendMagicEffect({
+						x = target_position.x + effect.x + rand.x,
+						y = target_position.y + effect.y + rand.y,
+						z = target_position.z
+					}, effect.id)
+				end
+
+
+				doCombat(cid, combat, numberToVariant(target))
+			end, loop * loop_ms_delay)
+		end
+	end,
+}
+
 function checkSpecialCooldown(cid)
 	if exhaustion.check(cid, "special") then
 		local exhaust = exhaustion.get(cid, "special")
