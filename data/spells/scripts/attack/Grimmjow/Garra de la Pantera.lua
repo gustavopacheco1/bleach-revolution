b1bd300f4e2098fdeb640c7d2dfa773a87f8793c
magojackson <97809271+magojackson@users.noreply.github.com>
2022-07-28 21:43:08 -0300

    edit garra de la pantera.lua

--- data/spells/scripts/attack/Grimmjow/Garra de la Pantera.lua
+++ data/spells/scripts/attack/Grimmjow/Garra de la Pantera.lua
@@ -5,13 +5,31 @@ setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, 23)
 setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -67.0, 0, -67.0, 0)
 
 function onCastSpell(cid, var)
-	if exhaustion.check(cid, 200) then
-		doPlayerSendCancel(cid, "You are exhausted.")
-		return false
-	end
+    if exhaustion.check(cid, 200) then
+        doPlayerSendCancel(cid, "You are exhausted.")
+        return false
+    end
 
-	CustomSpell.skyShoot(cid, combat, 3, 400, 23, { id = 23, x = 227, y = 227 })
+    if hasCreatureCondition(cid, CONDITION_ATTRIBUTES) then
+        CustomSpell.skyShoot(
+            cid,
+            combat,
+            4,
+            300,
+            91,
+            { id = 227, x = 1, y = 1 }
+        )
+    else
+        CustomSpell.randomEffect(
+            cid,
+            combat,
+            4,
+            300,
+            23,
+            { id = 466, x = 1, y = 1 }
+        )
+    end
 
-	exhaustion.set(cid, 200, 2)
-	return true
+    exhaustion.set(cid, 200, 2)
+    return true
 end
