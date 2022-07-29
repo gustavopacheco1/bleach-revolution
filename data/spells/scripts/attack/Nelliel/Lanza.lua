20c851e57652e953b94cc64d20ed93e6fe4d84a0
magojackson <97809271+magojackson@users.noreply.github.com>
2022-07-29 00:38:21 -0300

    edit lanza.lua

--- data/spells/scripts/attack/Nelliel/Lanza.lua
+++ data/spells/scripts/attack/Nelliel/Lanza.lua
@@ -1,24 +1,34 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_HITCOLOR, COLOR_PINK)
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
-setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, 16)
-setCombatParam(combat, COMBAT_PARAM_EFFECT, 487)
 setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -46.0, 0, -46.0, 0)
 
 function onCastSpell(cid, var)
-	if exhaustion.check(cid, 200) then
-		doPlayerSendCancel(cid, "You are exhausted.")
-		return false
-	end
+    if exhaustion.check(cid, 200) then
+        doPlayerSendCancel(cid, "You are exhausted.")
+        return false
+    end
 
-	for i = 0, 4 do
-		addEvent(function()
-			if isCreature(cid) then
-				doCombat(cid, combat, var)
-			end
-		end, i * 300)
-	end
+    if hasCreatureCondition(cid, CONDITION_ATTRIBUTES) then
+        CustomSpell.randomShoot(
+            cid,
+            combat,
+            4,
+            300,
+            0,
+            { id = 0, x = 0, y = 0 }
+        )
+    else
+        CustomSpell.randomShoot(
+            cid,
+            combat,
+            4,
+            300,
+            8,
+            { id = 229, x = 0, y = 0 }
+        )
+    end
 
-	exhaustion.set(cid, 200, 2)
-	return true
+    exhaustion.set(cid, 200, 2)
+    return true
 end
