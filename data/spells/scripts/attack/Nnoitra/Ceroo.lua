194c730180750592017ecfc9b5cd48c31991dc8f
magojackson <97809271+magojackson@users.noreply.github.com>
2022-07-29 00:13:36 -0300

    edit ceroo.lua

--- data/spells/scripts/attack/Nnoitra/Ceroo.lua
+++ data/spells/scripts/attack/Nnoitra/Ceroo.lua
@@ -1,37 +1,34 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_HITCOLOR, COLOR_PINK)
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
-setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, 89)
 setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -42.0, 0, -42.0, 0)
 
 function onCastSpell(cid, var)
-	if exhaustion.check(cid, 200) then
-		doPlayerSendCancel(cid, "You are exhausted.")
-		return false
-	end
+    if exhaustion.check(cid, 200) then
+        doPlayerSendCancel(cid, "You are exhausted.")
+        return false
+    end
 
-	for i = 0, 4 do
-		addEvent(function()
-			if isCreature(cid) then
-				local target = getCreatureTarget(cid)
+    if hasCreatureCondition(cid, CONDITION_ATTRIBUTES) then
+        CustomSpell.randomShoot(
+            cid,
+            combat,
+            4,
+            300,
+            90,
+            { id = 733, x = 1, y = 1 }
+        )
+    else
+        CustomSpell.randomShoot(
+            cid,
+            combat,
+            4,
+            300,
+            89,
+            { id = 697, x = 0, y = 0 }
+        )
+    end
 
-				if isCreature(target) then
-					local target_position = getCreaturePosition(target)
-
-					if isSightClear(getCreaturePosition(cid), target_position, false) then
-						doSendMagicEffect({
-							x = target_position.x + 2,
-							y = target_position.y + 2,
-							z = target_position.z
-						}, 740)
-
-						doCombat(cid, combat, numberToVariant(target))
-					end
-				end
-			end
-		end, i * 300)
-	end
-
-	exhaustion.set(cid, 200, 2)
-	return true
+    exhaustion.set(cid, 200, 2)
+    return true
 end
