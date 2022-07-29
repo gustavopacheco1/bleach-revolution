8b8498840a3aa79e82ce60d628ea73a710d792fa
magojackson <97809271+magojackson@users.noreply.github.com>
2022-07-28 22:06:45 -0300

    edit getsuga.lua

--- data/spells/scripts/attack/Ichigo/Getsuga.lua
+++ data/spells/scripts/attack/Ichigo/Getsuga.lua
@@ -1,24 +1,34 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_HITCOLOR, COLOR_PINK)
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
-setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, 6)
-setCombatParam(combat, COMBAT_PARAM_EFFECT, 484)
 setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -66.0, 0, -66.0, 0)
 
 function onCastSpell(cid, var)
-	if exhaustion.check(cid, 200) then
-		doPlayerSendCancel(cid, "You are exhausted.")
-		return false
-	end
+    if exhaustion.check(cid, 200) then
+        doPlayerSendCancel(cid, "You are exhausted.")
+        return false
+    end
 
-	for i = 0, 4 do
-		addEvent(function()
-			if isCreature(cid) then
-				doCombat(cid, combat, var)
-			end
-		end, i * 300)
-	end
+    if hasCreatureCondition(cid, CONDITION_ATTRIBUTES) then
+        CustomSpell.skyShoot(
+            cid,
+            combat,
+            4,
+            300,
+            6,
+            { id = 484, x = 1, y = 1 }
+        )
+    else
+        CustomSpell.randomEffect(
+            cid,
+            combat,
+            4,
+            300,
+            7,
+            { id = 242, x = 1, y = 1 }
+        )
+    end
 
-	exhaustion.set(cid, 200, 2)
-	return true
+    exhaustion.set(cid, 200, 2)
+    return true
 end
