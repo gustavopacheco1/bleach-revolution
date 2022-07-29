12aa1b2fcdb393d2c81146132fb3e6cb80fcaf7f
magojackson <97809271+magojackson@users.noreply.github.com>
2022-07-29 01:08:10 -0300

    edit el directo.lua

--- data/spells/scripts/attack/Chad/El Directo.lua
+++ data/spells/scripts/attack/Chad/El Directo.lua
@@ -1,24 +1,39 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_HITCOLOR, COLOR_PINK)
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
-setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, 44)
-setCombatParam(combat, COMBAT_PARAM_EFFECT, 530)
 setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -41.0, 0, -41.0, 0)
 
+local combat = createCombatObject()
+setCombatParam(combat, COMBAT_PARAM_HITCOLOR, COLOR_PINK)
+setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
+setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -66.0, 0, -66.0, 0)
+
 function onCastSpell(cid, var)
-	if exhaustion.check(cid, 200) then
-		doPlayerSendCancel(cid, "You are exhausted.")
-		return false
-	end
+    if exhaustion.check(cid, 200) then
+        doPlayerSendCancel(cid, "You are exhausted.")
+        return false
+    end
 
-	for i = 0, 4 do
-		addEvent(function()
-			if isCreature(cid) then
-				doCombat(cid, combat, var)
-			end
-		end, i * 300)
-	end
+    if hasCreatureCondition(cid, CONDITION_ATTRIBUTES) then
+        CustomSpell.randomShoot(
+            cid,
+            combat,
+            4,
+            300,
+            0,
+            { id = 0, x = 0, y = 0 }
+        )
+    else
+        CustomSpell.randomShoot(
+            cid,
+            combat,
+            4,
+            300,
+            44,
+            { id = 28, x = 0, y = 0 }
+        )
+    end
 
-	exhaustion.set(cid, 200, 2)
-	return true
+    exhaustion.set(cid, 200, 2)
+    return true
 end
