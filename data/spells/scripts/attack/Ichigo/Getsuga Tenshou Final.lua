49b6ad10ad0f77e3d420c211d252875cb7a1a617
magojackson <97809271+magojackson@users.noreply.github.com>
2022-07-30 17:44:00 -0300

    edit getsuga tenshou final.lua

--- data/spells/scripts/attack/Ichigo/Getsuga Tenshou Final.lua
+++ data/spells/scripts/attack/Ichigo/Getsuga Tenshou Final.lua
@@ -1,20 +1,34 @@
 local combat = createCombatObject()
+setCombatParam(combat, COMBAT_PARAM_HITCOLOR, COLOR_PINK)
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
 setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -120.0, 0, -120.0, 0)
 
 function onCastSpell(cid, var)
-	if exhaustion.check(cid, 270) then
-		doPlayerSendCancel(cid, "You are exhausted.")
-		return false
-	end
+    if exhaustion.check(cid, 270) then
+        doPlayerSendCancel(cid, "You are exhausted.")
+        return false
+    end
 
-	local target_position = getCreaturePosition(getCreatureTarget(cid))
-	doSendMagicEffect({
-		x = target_position.x + 3,
-		y = target_position.y,
-		z = target_position.z
-	}, 15)
+    if hasCreatureCondition(cid, CONDITION_ATTRIBUTES) then
+        CustomSpell.randomShoot(
+            cid,
+            combat,
+            1,
+            300,
+            92,
+            { id = 15, x = 0, y = 0 }
+        )
+    else
+        CustomSpell.randomShoot(
+            cid,
+            combat,
+            1,
+            300,
+            92,
+            { id = 15, x = 0, y = 0 }
+        )
+    end
 
-	exhaustion.set(cid, 270, 2)
-	return doCombat(cid, combat, var)
+    exhaustion.set(cid, 270, 2)
+    return true
 end
