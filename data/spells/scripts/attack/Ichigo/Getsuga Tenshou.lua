a49adeaaf1f25870cff45ffb491643c07036781e
magojackson <97809271+magojackson@users.noreply.github.com>
2022-07-30 13:43:55 -0300

    edit getsuga tenshou.lua

--- data/spells/scripts/attack/Ichigo/Getsuga Tenshou.lua
+++ data/spells/scripts/attack/Ichigo/Getsuga Tenshou.lua
@@ -1,21 +1,26 @@
 local combat = createCombatObject()
+setCombatParam(combat, COMBAT_PARAM_HITCOLOR, COLOR_TEAL)
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
-setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, 81)
-setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -115.0, 0, -115.0, 0)
+setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -115, 0, -115, 0)
+setCombatArea(combat, createCombatArea(AREA_CANNON6SQM, AREADIAGONAL_CANNON6SQM))
 
 function onCastSpell(cid, var)
-	if exhaustion.check(cid, 250) then
-		doPlayerSendCancel(cid, "You are exhausted.")
+	if exhaustion.check(cid, "cannon") then
+		doPlayerSendDefaulCancel(cid, RETURNVALUE_YOUAREEXHAUSTED)
 		return false
 	end
 
-	local target_position = getCreaturePosition(getCreatureTarget(cid))
-	doSendMagicEffect({
-		x = target_position.x + 1,
-		y = target_position.y + 1,
-		z = target_position.z
-	}, 174)
+	doCannon(
+		cid,
+		combat,
+		var,
+		6,
+		747,
+		747,
+		747,
+		747
+	)
 
-	exhaustion.set(cid, 250, 2)
-	return doCombat(cid, combat, var)
+	exhaustion.set(cid, "cannon", 2)
+	return true
 end
