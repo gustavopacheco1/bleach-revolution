59cbd9fca08aa43159da65e0fa9c3797143fb742
magojackson <97809271+magojackson@users.noreply.github.com>
2022-07-30 19:38:22 -0300

    edit spells ulquiorra

--- data/spells/scripts/attack/Ulquiorra/Cero Oscuras.lua
+++ data/spells/scripts/attack/Ulquiorra/Cero Oscuras.lua
@@ -1,39 +1,14 @@
 local combat = createCombatObject()
-setCombatParam(combat, COMBAT_PARAM_HITCOLOR, COLOR_PINK)
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
-setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -47.0, 0, -47.0, 0)
-
-local combat = createCombatObject()
-setCombatParam(combat, COMBAT_PARAM_HITCOLOR, COLOR_PINK)
-setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
-setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -66.0, 0, -66.0, 0)
+setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, 36)
+setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -106.0, 0, -106.0, 0)
 
 function onCastSpell(cid, var)
-    if exhaustion.check(cid, 200) then
-        doPlayerSendCancel(cid, "You are exhausted.")
-        return false
-    end
+	if exhaustion.check(cid, 270) then
+		doPlayerSendCancel(cid, "You are exhausted.")
+		return false
+	end
 
-    if hasCreatureCondition(cid, CONDITION_ATTRIBUTES) then
-        CustomSpell.randomShoot(
-            cid,
-            combat,
-            4,
-            300,
-            0,
-            { id = 0, x = 0, y = 0 }
-        )
-    else
-        CustomSpell.randomShoot(
-            cid,
-            combat,
-            4,
-            300,
-            60,
-            { id = 6, x = 0, y = 0 }
-        )
-    end
-
-    exhaustion.set(cid, 200, 2)
-    return true
+	exhaustion.set(cid, 270, 2)
+	return doCombat(cid, combat, var)
 end
---
+++ data/spells/scripts/attack/Ulquiorra/Lanza Del Relampago.lua
@@ -0,0 +1,39 @@
+local combat = createCombatObject()
+setCombatParam(combat, COMBAT_PARAM_HITCOLOR, COLOR_PINK)
+setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
+setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -47.0, 0, -47.0, 0)
+
+local combat = createCombatObject()
+setCombatParam(combat, COMBAT_PARAM_HITCOLOR, COLOR_PINK)
+setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
+setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -66.0, 0, -66.0, 0)
+
+function onCastSpell(cid, var)
+    if exhaustion.check(cid, 200) then
+        doPlayerSendCancel(cid, "You are exhausted.")
+        return false
+    end
+
+    if hasCreatureCondition(cid, CONDITION_ATTRIBUTES) then
+        CustomSpell.randomShoot(
+            cid,
+            combat,
+            4,
+            300,
+            0,
+            { id = 0, x = 0, y = 0 }
+        )
+    else
+        CustomSpell.randomShoot(
+            cid,
+            combat,
+            4,
+            300,
+            60,
+            { id = 6, x = 0, y = 0 }
+        )
+    end
+
+    exhaustion.set(cid, 200, 2)
+    return true
+end
