48abf0bbb49b052ef837dafbabbe18220a05d188
magojackson <97809271+magojackson@users.noreply.github.com>
2022-07-30 23:10:25 -0300

    edit doble cero.lua e cero.lua

--- data/spells/scripts/attack/Nelliel/Cero.lua
+++ data/spells/scripts/attack/Nelliel/Cero.lua
@@ -1,20 +1,23 @@
 local combat = createCombatObject()
+setCombatParam(combat, COMBAT_PARAM_HITCOLOR, COLOR_TEAL)
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
-setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -100.0, 0, -100.0, 0)
+setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -100, 0, -100, 0)
+setCombatArea(combat, createCombatArea(AREA_CANNON6SQM, AREADIAGONAL_CANNON6SQM))
 
 function onCastSpell(cid, var)
-	if exhaustion.check(cid, 250) then
-		doPlayerSendCancel(cid, "You are exhausted.")
+	if exhaustion.check(cid, "cannon") then
+		doPlayerSendDefaulCancel(cid, RETURNVALUE_YOUAREEXHAUSTED)
 		return false
 	end
 
-	local target_position = getCreaturePosition(getCreatureTarget(cid))
-	doSendMagicEffect({
-		x = target_position.x + 2,
-		y = target_position.y + 2,
-		z = target_position.z
-	}, 686)
+	doCannon(
+		cid,
+		combat,
+		var,
+		{ length = 6, height = 3 },
+		{ east = 754, west = 754, north = 753, south = 753 }
+	)
 
-	exhaustion.set(cid, 250, 2)
-	return doCombat(cid, combat, var)
+	exhaustion.set(cid, "cannon", 3)
+	return true
 end
