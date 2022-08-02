ef166a6e740d546990478fdb9ae1160e3840a66d
magojackson <97809271+magojackson@users.noreply.github.com>
2022-08-01 23:44:14 -0300

    edit garra de la pantera/gran rey cero

--- data/spells/scripts/attack/Grimmjow/Gran Rey Cero.lua
+++ data/spells/scripts/attack/Grimmjow/Gran Rey Cero.lua
@@ -1,21 +1,23 @@
 local combat = createCombatObject()
+setCombatParam(combat, COMBAT_PARAM_HITCOLOR, COLOR_TEAL)
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
-setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, 10)
-setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -116.0, 0, -131.0, 0)
+setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -116, 0, -131, 0)
+setCombatArea(combat, createCombatArea(AREA_CANNON6SQM, AREADIAGONAL_CANNON6SQM))
 
 function onCastSpell(cid, var)
-	if exhaustion.check(cid, 250) then
-		doPlayerSendCancel(cid, "You are exhausted.")
+	if exhaustion.check(cid, "cannon") then
+		doPlayerSendDefaulCancel(cid, RETURNVALUE_YOUAREEXHAUSTED)
 		return false
 	end
 
-	local target_position = getCreaturePosition(getCreatureTarget(cid))
-	doSendMagicEffect({
-		x = target_position.x,
-		y = target_position.y,
-		z = target_position.z
-	}, 482)
+	doCannon(
+		cid,
+		combat,
+		var,
+		{ length = 6, height = 3 },
+		{ east = 758, west = 758, north = 757, south = 757 }
+	)
 
-	exhaustion.set(cid, 250, 2)
-	return doCombat(cid, combat, var)
+	exhaustion.set(cid, "cannon", 3)
+	return true
 end
