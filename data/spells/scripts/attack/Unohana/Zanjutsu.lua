992c55d631261e4d61e03a1e6e91c60ee17bcdf4
magojackson <97809271+magojackson@users.noreply.github.com>
2022-08-04 02:02:00 -0300

    edit minazuki.lua, zanjutsu 2.lua e zanjutsu.lua

--- data/spells/scripts/attack/Unohana/Zanjutsu.lua
+++ data/spells/scripts/attack/Unohana/Zanjutsu.lua
@@ -4,11 +4,11 @@ setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, 19)
 setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -111.0, 0, -126.0, 0)
 
 function onCastSpell(cid, var)
-	if exhaustion.check(cid, 270) then
+	if exhaustion.check(cid, 300) then
 		doPlayerSendCancel(cid, "You are exhausted.")
 		return false
 	end
 
-	exhaustion.set(cid, 270, 2)
+	exhaustion.set(cid, 300, 3)
 	return doCombat(cid, combat, var)
 end
