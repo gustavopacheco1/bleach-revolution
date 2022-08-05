995ccb6c1c0ac09106c11a474c1d48745cfd0055
Gustavo <gustavopacheco61@yahoo.com>
2022-08-04 23:41:50 -0300

    Fix skill stages - Não dava pra usar numeros quebrados (0.5%) - Agora 10 equivale a 1%

--- data/creaturescripts/scripts/skills_stages.lua
+++ data/creaturescripts/scripts/skills_stages.lua
@@ -1,66 +1,68 @@
 local skills_stages = {
 	[SKILL_FIST] = {
-		{ level = 0, rate = 3 },
-		{ level = 60, rate = 2 },
-		{ level = 80, rate = 1 },
-		{ level = 100, rate = 0.8 },
-		{ level = 120, rate = 0.5 }
+		{ level = 0, rate = 30 },
+		{ level = 60, rate = 20 },
+		{ level = 80, rate = 10 },
+		{ level = 100, rate = 8 },
+		{ level = 120, rate = 5 }
 	},
 
 	[SKILL_CLUB] = {
-		{ level = 0, rate = 3 },
-		{ level = 60, rate = 2 },
-		{ level = 80, rate = 1 },
-		{ level = 100, rate = 0.8 },
-		{ level = 120, rate = 0.5 }
+		{ level = 0, rate = 30 },
+		{ level = 60, rate = 20 },
+		{ level = 80, rate = 10 },
+		{ level = 100, rate = 8 },
+		{ level = 120, rate = 5 }
 	},
 
 	[SKILL_SWORD] = {
-		{ level = 0, rate = 3 },
-		{ level = 60, rate = 2 },
-		{ level = 80, rate = 1 },
-		{ level = 100, rate = 0.8 },
-		{ level = 120, rate = 0.5 }
+		{ level = 0, rate = 30 },
+		{ level = 60, rate = 20 },
+		{ level = 80, rate = 10 },
+		{ level = 100, rate = 8 },
+		{ level = 120, rate = 5 }
 	},
 
 	[SKILL_AXE] = {
-		{ level = 0, rate = 3 },
-		{ level = 60, rate = 2 },
-		{ level = 80, rate = 1 },
-		{ level = 100, rate = 0.8 },
-		{ level = 120, rate = 0.5 }
+		{ level = 0, rate = 30 },
+		{ level = 60, rate = 20 },
+		{ level = 80, rate = 10 },
+		{ level = 100, rate = 8 },
+		{ level = 120, rate = 5 }
 	},
 
 	[SKILL_DISTANCE] = {
-		{ level = 0, rate = 3 },
-		{ level = 60, rate = 2 },
-		{ level = 80, rate = 1 },
-		{ level = 100, rate = 0.8 },
-		{ level = 120, rate = 0.5 }
+		{ level = 0, rate = 30 },
+		{ level = 60, rate = 20 },
+		{ level = 80, rate = 10 },
+		{ level = 100, rate = 8 },
+		{ level = 120, rate = 5 }
 	},
 
 	[SKILL_SHIELD] = {
-		{ level = 0, rate = 3 },
-		{ level = 60, rate = 2 },
-		{ level = 80, rate = 1 },
-		{ level = 100, rate = 0.8 },
-		{ level = 120, rate = 0.5 }
+		{ level = 0, rate = 30 },
+		{ level = 60, rate = 20 },
+		{ level = 80, rate = 10 },
+		{ level = 100, rate = 8 },
+		{ level = 120, rate = 5 }
 	},
 
 	[SKILL_FISHING] = {
-		{ level = 0, rate = 3 },
-		{ level = 60, rate = 2 },
-		{ level = 80, rate = 1 },
-		{ level = 100, rate = 0.8 },
-		{ level = 120, rate = 0.5 }
+		{ level = 0, rate = 10 },
+		{ level = 30, rate = 5 },
+		{ level = 60, rate = 1 },
+		{ level = 80, rate = 3 },
+		{ level = 100, rate = 2 },
+		{ level = 120, rate = 1 }
 	},
 
 	[SKILL__MAGLEVEL] = {
-		{ level = 0, rate = 3 },
-		{ level = 100, rate = 2 },
-		{ level = 120, rate = 1 },
-		{ level = 135, rate = 0.5 },
-		{ level = 140, rate = 0.1 },
+		{ level = 0, rate = 30 },
+		{ level = 30, rate = 25 },
+		{ level = 100, rate = 20 },
+		{ level = 120, rate = 10 },
+		{ level = 135, rate = 5 },
+		{ level = 140, rate = 1 },
 		{ level = 155, rate = 0 }
 	}
 }
@@ -74,7 +76,7 @@ function onLogin(cid)
 			skill_level = getPlayerMagLevel(cid, true)
 		end
 
-		local skill_rate
+		local skill_rate = 0
 		for _, skill_stage in pairs(skill_stages) do
 			if (skill_level < skill_stage.level) then
 				break
