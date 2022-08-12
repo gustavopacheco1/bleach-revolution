80a60fdd4c0505db0773820a7891adae264eb009
Gustavo <gustavopacheco61@yahoo.com>
2022-08-11 23:38:33 -0300

    Fix posição do teleport do mercado do urahara

--- data/actions/scripts/teleports/teleports.lua
+++ data/actions/scripts/teleports/teleports.lua
@@ -1,11 +1,24 @@
 local teleports = {
-	[20000] = { position = { x = 3587, y = 3463, z = 8 } },
-	[20001] = { position = { x = 3434, y = 3585, z = 8 }, level = 100 },
+	[20000] = { position = { x = 3587, y = 3462, z = 8 } },
+	[20001] = {
+		position = { x = 3434, y = 3585, z = 8 },
+		level = 100
+	},
 	[20002] = { position = { x = 2434, y = 3391, z = 7 }, level = 230 },
 	[20003] = { position = { x = 2433, y = 3395, z = 7 }, level = 230 },
-	[20004] = { position = { x = 3203, y = 2812, z = 15 }, storage = { key = "menus_florest_access", value = 1 }, effect = {id = 111, x = 1, y = 0} },
-	[20005] = { position = { x = 3371, y = 4116, z = 7 }, level = 230 },
-	[20006] = { position = { x = 3379, y = 4115, z = 7 }, level = 230 },
+	[20004] = {
+		position = { x = 3203, y = 2812, z = 15 },
+		storage = { key = "menus_florest_access", value = 1 },
+		effect = { id = 111, x = 1, y = 0 }
+	},
+	[20005] = {
+		position = { x = 3371, y = 4116, z = 7 },
+		level = 230
+	},
+	[20006] = {
+		position = { x = 3379, y = 4115, z = 7 },
+		level = 230
+	},
 }
 
 function onUse(cid, item, fromPosition, itemEx, toPosition)
