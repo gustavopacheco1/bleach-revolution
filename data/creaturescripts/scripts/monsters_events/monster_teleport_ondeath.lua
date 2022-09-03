42f4d58933ead4b50db4d33b27256c6fbd832401
Gustavo <gustavopacheco61@yahoo.com>
2022-09-03 18:48:37 -0300

    Update monsters - Remove monstros não utilizados - Melhora o nome

--- data/creaturescripts/scripts/monsters_events/monster_teleport_ondeath.lua
+++ data/creaturescripts/scripts/monsters_events/monster_teleport_ondeath.lua
@@ -1,18 +1,11 @@
 local bosses = {
-	["Samurai Boss"] = { teleport_to = { x = 3330, y = 3627, z = 8 } },
-	["Kensei Boss"] = { teleport_to = { x = 3895, y = 3416, z = 6 } },
-	["Especial Hollow Snake"] = { teleport_to = { x = 3228, y = 3207, z = 8 } },
-	["Barragan Boss"] = { teleport_to = { x = 4284, y = 3417, z = 6 } },
-	["Elite Grand Fisher"] = { storage = "tessai_quest", teleport_to = { x = 3583, y = 3456, z = 8 } },
+	["Super Samurai"] = { teleport_to = { x = 3330, y = 3627, z = 8 } },
+	["Super Acidwire"] = { teleport_to = { x = 3228, y = 3207, z = 8 } },
+	["Ultimate Grand Fisher"] = { storage = "tessai_quest", teleport_to = { x = 3583, y = 3456, z = 8 } },
 	["Zaraki Legendary"] = { teleport_to = { x = 2769, y = 3937, z = 4 } },
-	["Arrancar Boss"] = { teleport_to = { x = 3651, y = 4230, z = 8 } },
-	["Ryu Boss"] = { teleport_to = { x = 3934, y = 3816, z = 8 } },
-	["Hiroshi Boss"] = { teleport_to = { x = 2789, y = 3234, z = 6 } },
-	["Elite Hiroshi Boss"] = { teleport_to = { x = 2789, y = 3294, z = 6 } },
-	["Vizard One Boss"] = { teleport_to = { x = 4502, y = 3762, z = 7 } },
-	["Vizard Two Boss"] = { teleport_to = { x = 4449, y = 3874, z = 7 } },
-	["Vizard Three Boss"] = { teleport_to = { x = 4541, y = 3956, z = 7 } },
-	["Vizard Four Boss"] = { teleport_to = { x = 4670, y = 3899, z = 7 } }
+	["Super Arrancar"] = { teleport_to = { x = 3651, y = 4230, z = 8 } },
+	["Super Hiroshi"] = { teleport_to = { x = 2789, y = 3234, z = 6 } },
+	["Ultimate Hiroshi"] = { teleport_to = { x = 2789, y = 3294, z = 6 } },
 }
 
 function onDeath(cid, corpse, deathList)
