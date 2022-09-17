558e9f621f1d82ba19bc9f5c0706a74f19126a69
Gustavo <gustavopacheco61@yahoo.com>
2022-09-17 19:34:16 -0300

    Refatora area spells

--- data/spells/scripts/attack/Area/Juuhaku Wave.lua
+++ data/spells/scripts/attack/Area/Juuhaku Wave.lua
@@ -1,88 +1,27 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
-setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -80.0, 0, -100.0, 0)
+setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -85.0, 0, -100.0, 0)
+setCombatArea(combat, createCombatArea(AREA_CIRCLE3X3_FULL))
 
-local combat1 = createCombatObject()
-setCombatParam(combat1, COMBAT_PARAM_EFFECT, 583)
+local area1 = createCombatObject()
+setCombatParam(area1, COMBAT_PARAM_EFFECT, 583)
+setCombatArea(area1, createCombatArea(AREA_CIRCLE3X3_FIRST))
 
-local combat2 = createCombatObject()
-setCombatParam(combat2, COMBAT_PARAM_EFFECT, 583)
+local area2 = createCombatObject()
+setCombatParam(area2, COMBAT_PARAM_EFFECT, 583)
+setCombatArea(area2, createCombatArea(AREA_CIRCLE3X3_SECOND))
 
-local combat3 = createCombatObject()
-setCombatParam(combat3, COMBAT_PARAM_EFFECT, 583)
-
-local combat4 = createCombatObject()
-setCombatParam(combat3, COMBAT_PARAM_EFFECT, 583)
-
-local area_damage = {
-	{ 0, 0, 0, 0, 1, 0, 0, 0, 0, },
-	{ 0, 0, 0, 1, 1, 1, 0, 0, 0, },
-	{ 0, 0, 1, 1, 1, 1, 1, 0, 0, },
-	{ 0, 1, 1, 1, 1, 1, 1, 1, 0, },
-	{ 1, 1, 1, 1, 2, 1, 1, 1, 1, },
-	{ 0, 1, 1, 1, 1, 1, 1, 1, 0, },
-	{ 0, 0, 1, 1, 1, 1, 1, 0, 0, },
-	{ 0, 0, 0, 1, 1, 1, 0, 0, 0, },
-	{ 0, 0, 0, 0, 1, 0, 0, 0, 0, },
-	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, },
-}
-
-local area_1 = {
-	{ 0, 1, 0 },
-	{ 1, 2, 1 },
-	{ 0, 1, 0 },
-}
-
-local area_2 = {
-	{ 0, 0, 1, 0, 0 },
-	{ 0, 1, 0, 1, 0 },
-	{ 1, 0, 2, 0, 1 },
-	{ 0, 1, 0, 1, 0 },
-	{ 0, 0, 1, 0, 0 },
-}
-
-local area_3 = {
-	{ 0, 0, 0, 1, 0, 0, 0 },
-	{ 0, 0, 1, 0, 1, 0, 0 },
-	{ 0, 1, 0, 0, 0, 1, 0 },
-	{ 1, 0, 0, 2, 0, 0, 1 },
-	{ 0, 1, 0, 0, 0, 1, 0 },
-	{ 0, 0, 1, 0, 1, 0, 0 },
-	{ 0, 0, 0, 1, 0, 0, 0 },
-}
-
-local area_4 = {
-	{ 0, 0, 0, 0, 1, 0, 0, 0, 0 },
-	{ 0, 0, 0, 1, 0, 1, 0, 0, 0 },
-	{ 0, 0, 1, 0, 0, 0, 1, 0, 0 },
-	{ 0, 1, 0, 0, 0, 0, 0, 1, 0 },
-	{ 1, 0, 0, 0, 2, 0, 0, 0, 1 },
-	{ 0, 1, 0, 0, 0, 0, 0, 1, 0 },
-	{ 0, 0, 1, 0, 0, 0, 1, 0, 0 },
-	{ 0, 0, 0, 1, 0, 1, 0, 0, 0 },
-	{ 0, 0, 0, 0, 1, 0, 0, 0, 0 },
-}
-
-setCombatArea(combat, createCombatArea(area_damage))
-setCombatArea(combat1, createCombatArea(area_1))
-setCombatArea(combat2, createCombatArea(area_2))
-setCombatArea(combat3, createCombatArea(area_3))
-setCombatArea(combat4, createCombatArea(area_4))
+local area3 = createCombatObject()
+setCombatParam(area3, COMBAT_PARAM_EFFECT, 583)
+setCombatArea(area3, createCombatArea(AREA_CIRCLE3X3_THIRD))
 
 function onCastSpell(cid, var)
+	-- Damage
 	doCombat(cid, combat, var)
-	doCombat(cid, combat1, var)
 
-	addEvent(function()
-		doCombat(cid, combat2, var)
-	end, 150)
-
-	addEvent(function()
-		doCombat(cid, combat3, var)
-	end, 300)
-
-	addEvent(function()
-		doCombat(cid, combat4, var)
-	end, 450)
+	-- Effects
+	doCombat(cid, area1, var)
+	addEvent(doCombat, 75, cid, area2, var)
+	addEvent(doCombat, 150, cid, area3, var)
 	return true
 end
