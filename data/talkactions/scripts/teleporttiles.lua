9e19ae09b08da4a3eba979f8ab4c743b56bfe32b
Gustavo <gustavopacheco61@yahoo.com>
2022-09-18 15:30:16 -0300

    Update `/a` talkaction

--- data/talkactions/scripts/teleporttiles.lua
+++ data/talkactions/scripts/teleporttiles.lua
@@ -18,18 +18,13 @@ function onSay(cid, words, param, channel)
 		end
 	end
 
-	local pos = getClosestFreeTile(pid, getPosByDir(getCreaturePosition(pid), getCreatureLookDirection(pid), n), false,
-		false)
+	local pos = getClosestFreeTile(pid, getPosByDir(getCreaturePosition(pid), getCreatureLookDirection(pid), n), false, false)
 	if (not pos or isInArray({ pos.x, pos.y }, 0)) then
 		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Destination not reachable.")
 		return true
 	end
 
-	local tmp = getCreaturePosition(pid)
-	if (doTeleportThing(pid, pos, true) and not isPlayerGhost(pid)) then
-		doSendMagicEffect(tmp, CONST_ME_POFF)
-		doSendMagicEffect(pos, CONST_ME_TELEPORT)
-	end
+	doTeleportThing(pid, pos, false)
 
 	return true
 end
