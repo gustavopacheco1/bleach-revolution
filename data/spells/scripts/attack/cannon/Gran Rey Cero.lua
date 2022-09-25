377745ba01edc8ab486120e0670876f29bea8327
Gustavo <gustavopacheco61@yahoo.com>
2022-09-25 14:10:04 -0300

    Organiza cannons e remove terceira spell de target

--- data/spells/scripts/attack/cannon/Gran Rey Cero.lua
+++ data/spells/scripts/attack/cannon/Gran Rey Cero.lua
@@ -5,20 +5,40 @@ setCombatArea(combat, createCombatArea(AREA_BIG_CANNON_6SQM, AREADIAGONAL_BIG_CA
 onGetFormulaValues = getCannonFormula
 setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
+local vocations_effects = {
+	-- Nnoitra Gilga
+	[21] = {
+		east = 618,
+		west = 618,
+		north = 617,
+		south = 617,
+	},
+
+	-- Grimmjow Jaegerjaquez
+	[18] = {
+		east = 758,
+		west = 758,
+		north = 757,
+		south = 757,
+	}
+}
+
 function onCastSpell(cid, var)
-	if exhaustion.check(cid, "cannon") then
+	if exhaustion.check(cid, "special") then
 		doPlayerSendDefaultCancel(cid, RETURNVALUE_YOUAREEXHAUSTED)
 		return false
 	end
+
+	local vocation_effects = vocations_effects[getPlayerVocation(cid)]
 
 	doCannon(
 		cid,
 		combat,
 		var,
 		{ length = 6, height = 3 },
-		{ east = 758, west = 758, north = 757, south = 757 }
+		{ east = vocation_effects.east, west = vocation_effects.west, north = vocation_effects.north, south = vocation_effects.south }
 	)
 
-	exhaustion.set(cid, "cannon", COOLDOWN_CANNON)
+	exhaustion.set(cid, "special", COOLDOWN_CANNON)
 	return true
 end
