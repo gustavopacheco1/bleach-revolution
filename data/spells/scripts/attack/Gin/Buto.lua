227f137fb360c218fe859b1b523ecfadcc34f44a
magojackson <97809271+magojackson@users.noreply.github.com>
2022-09-25 14:32:01 -0300

    remove spells para balanceamento

--- data/spells/scripts/attack/Gin/Buto.lua
+++ data/spells/scripts/attack/Gin/Buto.lua
@@ -1,27 +1,25 @@
 local combat = createCombatObject()
+setCombatParam(combat, COMBAT_PARAM_HITCOLOR, COLOR_PINK)
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
-setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -106.0, 0, -106.0, 0)
+setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, 69)
+onGetFormulaValues = getSingleShootFormula
+setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
 function onCastSpell(cid, var)
-	if exhaustion.check(cid, 250) then
+	if exhaustion.check(cid, 270) then
 		doPlayerSendCancel(cid, "You are exhausted.")
 		return false
 	end
 
 	local target_position = getCreaturePosition(getCreatureTarget(cid))
 	doSendMagicEffect({
-		x = target_position.x,
-		y = target_position.y,
-		z = target_position.z
-	}, 544)
-
-	local target_position = getCreaturePosition(getCreatureTarget(cid))
-	doSendMagicEffect({
 		x = target_position.x + 2,
-		y = target_position.y + 1,
+		y = target_position.y + 2,
 		z = target_position.z
-	}, 549)
+	}, 551)
 
-	exhaustion.set(cid, 250, 2)
-	return doCombat(cid, combat, var)
+	doCombat(cid, combat, var)
+
+	exhaustion.set(cid, 270, 2)
+	return true
 end
