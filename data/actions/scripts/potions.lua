2368d7d2e999d9fc6372cceebeab49a538e6a4a5
Gustavo <gustavopacheco61@yahoo.com>
2022-10-11 08:46:44 -0300

    Add potions level

--- data/actions/scripts/potions.lua
+++ data/actions/scripts/potions.lua
@@ -1,24 +1,24 @@
 local potions = {
 	-- ultimate potion
-	[16112] = { heal = { health = 9000, mana = 9000, randomizer = 1000 }, effect = 502 },
+	[16112] = { level = 180, heal = { health = 9000, mana = 9000, randomizer = 1000 }, effect = 502 },
 
 	-- reiatsu potion
-	[16113] = { heal = { mana = 9000, randomizer = 1000 }, effect = 502 },
+	[16113] = { level = 150, heal = { mana = 9000, randomizer = 1000 }, effect = 502 },
 
 	-- health potion
-	[16114] = { heal = { health = 9000, randomizer = 1000 }, effect = 502 },
+	[16114] = { level = 150, heal = { health = 9000, randomizer = 1000 }, effect = 502 },
 
 	-- reiatsu pill
-	[16116] = { heal = { mana = 4500, randomizer = 500 }, effect = 502 },
+	[16116] = { level = 1, heal = { mana = 4500, randomizer = 500 }, effect = 502 },
 
 	-- health pill
-	[16117] = { heal = { health = 4500, randomizer = 500 }, effect = 502 },
+	[16117] = { level = 1, heal = { health = 4500, randomizer = 500 }, effect = 502 },
 
 	-- great health potion
-	[16118] = { heal = { health = 15000, randomizer = 2500 }, effect = 502 },
+	[16118] = { level = 200, heal = { health = 15000, randomizer = 2500 }, effect = 502 },
 
 	-- great reiatsu potion
-	[16111] = { heal = { mana = 15000, randomizer = 2500 }, effect = 502 },
+	[16111] = { level = 200, heal = { mana = 15000, randomizer = 2500 }, effect = 502 },
 }
 
 local exhaust = createConditionObject(CONDITION_EXHAUST)
@@ -27,6 +27,14 @@ setConditionParam(exhaust, CONDITION_PARAM_TICKS, 900)
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	if isPlayer(itemEx.uid) and not hasCondition(cid, CONDITION_EXHAUST) then
 		local potion = potions[item.itemid]
+
+		if getPlayerLevel(cid) < potion.level then
+			return MultiLanguage.doPlayerSendCancel(
+				cid,
+				"You need to be level " .. potion.level .. " to use this potion.",
+				"Você precisa ser nível " .. potion.level .. " para utilizar esta poção."
+			)
+		end
 
 		if potion.heal.health then
 			doCreatureAddHealth(itemEx.uid, math.random(potion.heal.health - potion.heal.randomizer, potion.heal.health + potion.heal.randomizer))
