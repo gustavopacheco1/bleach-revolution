1edc45320798b8ebaa045834506e41571b714961
magojackson <97809271+magojackson@users.noreply.github.com>
2022-10-27 01:52:45 -0300

    spells: edit black wave

--- data/spells/scripts/attack/Area/Black Wave.lua
+++ data/spells/scripts/attack/Area/Black Wave.lua
@@ -5,15 +5,15 @@ onGetFormulaValues = getAreaFormula
 setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
 local area1 = createCombatObject()
-setCombatParam(area1, COMBAT_PARAM_EFFECT, 759)
+setCombatParam(area1, COMBAT_PARAM_EFFECT, 479)
 setCombatArea(area1, createCombatArea(AREA_CIRCLE3X3_FIRST))
 
 local area2 = createCombatObject()
-setCombatParam(area2, COMBAT_PARAM_EFFECT, 759)
+setCombatParam(area2, COMBAT_PARAM_EFFECT, 479)
 setCombatArea(area2, createCombatArea(AREA_CIRCLE3X3_SECOND))
 
 local area3 = createCombatObject()
-setCombatParam(area3, COMBAT_PARAM_EFFECT, 759)
+setCombatParam(area3, COMBAT_PARAM_EFFECT, 479)
 setCombatArea(area3, createCombatArea(AREA_CIRCLE3X3_THIRD))
 
 function onCastSpell(cid, var)
